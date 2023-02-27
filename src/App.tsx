c11958203d5c8072fb0cfac11b0d8aaaa0c243da
subhonbek <basubhonbek@gmail.com>
2023-02-27 12:10:03 +0300

    zzz

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import './App.scss';
-import {Routes, Route} from 'react-router-dom';
-import RegComponent from './Components/Registration/Reg';
-import AutorisComponent from './Components/Autorisation/Autoris';
-import WelcomeComponent from './Components/Welcome/Welcome';
-import ChangePassComponent from './Components/ChangePassword/ChangePass';
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./zzz/login/Login";
+import { Registration } from "./zzz/registration/Registration";
 
 const App = () => {
   return (
-    <div className="Main">
-      <Routes>
-        <Route path="/registration/*" element={<RegComponent />}/>
-        <Route path="/*" element={<AutorisComponent />}/>
-        <Route path="/welcome/*" element={<WelcomeComponent />}/>
-        <Route path='/change_pass/*' element={<ChangePassComponent />}/>
-      </Routes>
-    </div>
+    <>
+      {/*<div className="Main">
+        <Routes>
+          <Route path="/registration/*" element={<RegComponent />} />
+          <Route path="/*" element={<AutorisComponent />} />
+          <Route path="/welcome/*" element={<WelcomeComponent />} />
+          <Route path="/change_pass/*" element={<ChangePassComponent />} />
+        </Routes>
+      </div>*/}
+      <main>
+        <Routes>
+          <Route path="/*" element={<Login />} />
+          <Route path="/registration/*" element={<Registration />} />
+        </Routes>
+      </main>
+    </>
   );
 };
-
 
 export default App;
---
+++ src/zzz/registration/Registration.tsx
@@ -0,0 +1,81 @@
+import { FC } from "react";
+import { Button, Form, Input, Typography } from "antd";
+
+import styles from "./Registration.module.scss";
+import CircleSVG from "../../assets/svg/CircleSVG";
+import CloseSVG from "../../assets/svg/CloseSVG";
+
+const { Title } = Typography;
+
+export const Registration: FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.svg}>
+        <CircleSVG />
+      </div>
+      <Form
+        name="login"
+        className={styles.loginForm}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <div className={styles.wrapper}>
+          <div className={styles.switcher}>
+            <span>EN </span>
+            <span className={styles.select}>РУС</span>
+          </div>
+          <CloseSVG />
+        </div>
+
+        <Title className={styles.title}>Регистрация</Title>
+
+        <Form.Item
+          name="surname"
+          rules={[
+            { type: "string" },
+            { required: true, message: "Please input your Surname!" },
+          ]}
+        >
+          <Input size="large" placeholder="Фамилия" />
+        </Form.Item>
+
+        <Form.Item
+          name="name"
+          rules={[
+            { type: "string" },
+            { required: true, message: "Please input your Name!" },
+          ]}
+        >
+          <Input size="large" placeholder="Имя" />
+        </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[
+            { type: "email" },
+            { required: true, message: "Please input your Email!" },
+          ]}
+        >
+          <Input size="large" placeholder="Логин / Email" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input size="large" type="password" placeholder="Пароль" />
+        </Form.Item>
+        <Form.Item>
+          <div className={styles.buttons}>
+            <Button size="large" type="primary" htmlType="submit">
+              Войти
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};

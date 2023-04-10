8455d5cab4b4f9327deb6375ccae563e4ba0d105
subhonbek <basubhonbek@gmail.com>
2023-04-10 10:33:25 +0300

    fix width

--- src/components/dnd/constatant.ts
+++ src/components/dnd/constatant.ts
@@ -1,208 +1,72 @@
 export const block = {
 	lg: [
 		{
-			w: 2,
-			h: 4,
+			w: 1,
+			h: 1,
+			maxH: 5,
 			x: 4,
 			y: 0,
 			i: '0',
-			maxH: 4,
 			moved: false,
 			static: false
 		},
 		{
 			w: 2,
-			h: 3,
+			maxH: 5,
+			h: 2,
 			x: 2,
 			y: 0,
 			i: '1',
-			maxH: 4,
+			moved: false,
+			static: false
+		},
+		{
+			w: 1,
+			h: 1,
+			maxH: 5,
+			x: 3,
+			y: 2,
+			i: '2',
+			moved: false,
+			static: false
+		},
+		{
+			w: 1,
+			maxH: 5,
+			h: 1,
+			x: 5,
+			y: 0,
+			i: '3',
 			moved: false,
 			static: false
 		},
 		{
 			w: 2,
 			h: 2,
+			maxH: 5,
 			x: 0,
 			y: 0,
-			i: '2',
-			maxH: 4,
+			i: '4',
 			moved: false,
 			static: false
 		},
 		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 2,
-			i: '3',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
+			w: 3,
 			h: 1,
-			x: 2,
+			x: 0,
+			maxH: 5,
+			y: 2,
+			i: '5',
+			moved: false,
+			static: false
+		},
+		{
+			w: 1,
+			h: 1,
+			maxH: 5,
+			x: 0,
 			y: 3,
-			i: '4',
-			maxH: 4,
-			moved: false,
-			static: false
-		}
-	],
-	md: [
-		{
-			w: 2,
-			h: 4,
-			x: 4,
-			y: 0,
-			i: '0',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 3,
-			x: 2,
-			y: 0,
-			i: '1',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 0,
-			i: '2',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 2,
-			i: '3',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 1,
-			x: 2,
-			y: 3,
-			i: '4',
-			maxH: 4,
-			moved: false,
-			static: false
-		}
-	],
-	sm: [
-		{
-			w: 2,
-			h: 4,
-			x: 2,
-			y: 0,
-			i: '0',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 3,
-			x: 2,
-			y: 4,
-			i: '1',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 0,
-			i: '2',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 2,
-			i: '3',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 1,
-			x: 2,
-			y: 7,
-			i: '4',
-			maxH: 4,
-			moved: false,
-			static: false
-		}
-	],
-	xs: [
-		{
-			w: 2,
-			h: 4,
-			x: 2,
-			y: 0,
-			i: '0',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 3,
-			x: 2,
-			y: 4,
-			i: '1',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 0,
-			i: '2',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 2,
-			x: 0,
-			y: 2,
-			i: '3',
-			maxH: 4,
-			moved: false,
-			static: false
-		},
-		{
-			w: 2,
-			h: 1,
-			x: 2,
-			y: 7,
-			i: '4',
-			maxH: 4,
+			i: '6',
 			moved: false,
 			static: false
 		}

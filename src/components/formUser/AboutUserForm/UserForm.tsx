b8fbd01da4e60fe49fcf1473a15f9c4ff9933f7f
MrSubhonbek <basubhonbek@gmail.com>
2023-06-22 16:42:43 +0300

    fix font size

--- src/components/formUser/AboutUserForm/UserForm.tsx
+++ src/components/formUser/AboutUserForm/UserForm.tsx
@@ -22,7 +22,7 @@ export const FormModal = () => {
 	return (
 		<ImagesLayout>
 			<div className="w-full flex justify-center ">
-				<div className="container max-w-2xl flex flex-col items-center justify-center  p-5">
+				<div className="container max-w-2xl flex flex-col items-center justify-center  px-5">
 					<h2 className="text-center font-bold text-2xl">Заполните короткую форму</h2>
 
 					<p className="mt-5 text-center text-sm font-bold px-7">

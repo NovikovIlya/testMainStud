a6b2d222713c0c7e1ef57f8a839307cc68b012f9
MrSubhonbek <basubhonbek@gmail.com>
2023-06-23 14:32:41 +0300

    add parent page form

--- src/components/formUser/ParentForm/ParentForm.tsx
+++ src/components/formUser/ParentForm/ParentForm.tsx
@@ -30,16 +30,24 @@ export const ParentForm = () => {
 					<Input
 						placeholder="Безухов Пьер Кириллович"
 						size="large"
-						className="mt-2"
+						className="mt-2 shadow"
 					/>
 					<div className="text-black text-[14px] font-normal mt-4">
 						Номер телефона родителя
 					</div>
-					<Input placeholder="+7 999 898-88-00" size="large" className="mt-2" />
+					<Input
+						placeholder="+7 999 898-88-00"
+						size="large"
+						className="mt-2 shadow"
+					/>
 					<div className="text-black text-[14px] font-normal mt-4">
 						Email родителя
 					</div>
-					<Input placeholder="BezuPr@gmail.com" size="large" className="mt-2" />
+					<Input
+						placeholder="BezuPr@gmail.com"
+						size="large"
+						className="mt-2 shadow"
+					/>
 					<div className="text-black text-[20px] mt-7 font-bold">
 						Документы родителя
 					</div>
@@ -47,7 +55,7 @@ export const ParentForm = () => {
 						Тип документа
 					</div>
 					<Select
-						className="mt-2 w-full"
+						className="mt-2 w-full shadow rounded-lg"
 						size="large"
 						options={[
 							{ value: 'паспорт' },
@@ -58,20 +66,61 @@ export const ParentForm = () => {
 					<div className="w-[151px] h-[19px] text-black text-[14px] font-bold mt-4">
 						Данные документа
 					</div>
-					<div className="grid grid-cols-2">
+					<div className="grid grid-cols-2 w-full gap-4">
 						<div className="mt-4">
 							<div className=" text-black text-[14px] font-normal">
 								Код подразделения
 							</div>
-							<Input placeholder="000-000" size="large" className="mt-2" />
+							<Input
+								placeholder="000-000"
+								size="large"
+								className="mt-2 shadow"
+							/>
 						</div>
 						<div className="mt-4">
 							<div className="text-black text-[14px] font-normal">
 								Когда выдан
 							</div>
-							<Input placeholder="000-000" size="large" className="mt-2" />
+							<Input placeholder="0000" size="large" className="mt-2 shadow" />
+						</div>
+						<div className="">
+							<div className="text-black text-[14px] font-normal">Серия</div>
+							<Input
+								placeholder="000-000"
+								size="large"
+								className="mt-2 shadow"
+							/>
+						</div>
+						<div className="">
+							<div className="text-black text-[14px] font-normal">Номер</div>
+							<Input placeholder="0000" size="large" className="mt-2 shadow" />
 						</div>
 					</div>
+					<div className="text-black text-[14px] mt-4 font-bold">
+						Дополнительные документы
+					</div>
+					<div className="text-black text-[14px] font-normal mt-4">СНИЛС</div>
+					<Input placeholder="0000" size="large" className="mt-2 shadow" />
+					<div className="text-black text-[14px] font-normal mt-4">ИНН</div>
+					<Input placeholder="0000" size="large" className="mt-2 shadow" />
+					<div className="text-black text-[20px] font-bold mt-7">Адрес</div>
+					<div className="text-black text-[14px] font-normal mt-7">
+						Адрес регистрации
+					</div>
+					<Input
+						placeholder="РФ, г. Казань, ул. Адорацкого, д.3, кв. 88"
+						size="large"
+						className="mt-2 shadow"
+					/>
+					<div className="text-black text-[14px] mt-4 font-normal">
+						Адрес проживания
+					</div>
+					<Input
+						placeholder="РФ, г. Казань, ул. Адорацкого, д.3, кв. 88"
+						size="large"
+						className="mt-2 shadow"
+					/>
+
 					<div className="w-full flex justify-center items-center gap-8 mt-[60px]">
 						<Button
 							onClick={handleCancel}

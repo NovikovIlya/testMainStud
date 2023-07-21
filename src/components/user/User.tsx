9252ebde262647c4639b0244ab0dc1808e577909
Subhonbek <basubhonbek@gmail.com>
2023-07-21 14:53:47 +0300

    fix bugs

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,5 +1,7 @@
+import clsx from 'clsx'
 import { useState } from 'react'
 
+import { useAppSelector } from '../../store'
 import DropDrag from '../dnd/DropDrag'
 import { block } from '../dnd/constant'
 import { Faq } from '../faq/Faq'
@@ -12,10 +14,22 @@ export const User = () => {
 			? JSON.parse(localStorage.getItem('dashboard') || '')
 			: block
 	})
+	const user = useAppSelector(state => state.InfoUser.role)
 	return (
 		<Layout>
-			<Heading className="ml-[180px] mt-[125px] max-sm:ml-3 text-2xl font-bold text-[#1F5CB8]">
-				Личный кабинет КФУ
+			<Heading
+				className={clsx(
+					`ml-[180px] mt-[125px] max-sm:ml-3 text-2xl font-bold text-[#1F5CB8]`,
+					user === 'guest' && 'bg-black',
+					user === 'enrollee' && 'bg-red-500',
+					user === 'student' && 'bg-yellow-500',
+					user === 'schoolboy' && 'bg-green-500',
+					user === 'listener' && 'bg-blue-500',
+					user === 'applicant' && 'bg-purple-500'
+				)}
+			>
+				Личный кабинет КФУ <br />
+				{user}
 			</Heading>
 			<DropDrag edit={true} layouts={layouts} setLayouts={setLayouts} />
 			<Faq />

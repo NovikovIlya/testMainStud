868b6f73bcc28e3d0c7dd6103ba62799a61e9971
Albert Tazetdinov <zet565758@gmail.com>
2024-03-04 22:04:22 +0300

    добавляю форму

--- src/components/service/businessTrip/NewBusinessTrip/NewBusinessTrip.tsx
+++ src/components/service/businessTrip/NewBusinessTrip/NewBusinessTrip.tsx
@@ -5,7 +5,7 @@ import {EmployeeDataPageForm} from "./employeeDataPageForm/employeeDataPageForm"
 
 export const NewBusinessTrip = () => {
     return (
-        <section className={'flex flex-col gap-10'}>
+        <section className={'flex flex-col gap-5'}>
             <span className={'text-2xl'}>Новая командировка</span>
             <ProgressForm step={1}/>
             <EmployeeDataPageForm/>
--- src/components/service/businessTrip/NewBusinessTrip/employeeDataPageForm/employeeDataPageForm.tsx
+++ src/components/service/businessTrip/NewBusinessTrip/employeeDataPageForm/employeeDataPageForm.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import {Form, Input, Select} from "antd";
 
 export const EmployeeDataPageForm = () => {
     return (
-        <form className={''}>
+        <div className={'flex flex-col gap-5'}>
+            <span className={'text-xl'}>Данные о сотруднике</span>
+            <Form layout={'vertical'} className={'flex gap-x-5'}>
+                <Form.Item
+                    label={<span className={'text-lg'}>ФИО сотрудника</span>}
+                    className={'w-full'}>
+                    <Input className={'text-base'}/>
+                </Form.Item>
 
-        </form>
+                <Form.Item
+                    label={<span className={'text-lg'}>Должность</span>}
+                    className={'w-full'}>
+                    <Select placeholder={'Выбрать'}>
+                        <Select.Option value={'test1'}>Тест 1</Select.Option>
+                        <Select.Option value={'test2'}>Тест 2</Select.Option>
+                        <Select.Option value={'test3'}>Тест 3</Select.Option>
+                    </Select>
+                </Form.Item>
+
+                {/*<Form.Item*/}
+                {/*    label={<span className={'text-lg'}>Номер телефона</span>}*/}
+                {/*    className={'w-2/5'}*/}
+                {/*>*/}
+                {/*    <Input className={'text-base'}/>*/}
+                {/*</Form.Item>*/}
+            </Form>
+        </div>
     );
 };
 

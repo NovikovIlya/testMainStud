e6ec5c5f7dd0457471bc482920d59caaadc69052
Albert Tazetdinov <zet565758@gmail.com>
2024-04-09 14:03:10 +0300

    убрал командировки с прода

--- src/components/service/index.tsx
+++ src/components/service/index.tsx
@@ -21,7 +21,6 @@ export const Service = () => {
 				{pathname.includes('/services/electronicBook') && <NavElectronicBook />}
 				{pathname.includes('/services/setting') && <NavSetting />}
 				{pathname.includes('/services/practices') && <NavPractices />}
-				{pathname.includes('/services/businessTrip') && <NavBusinessTrip />}
 				{pathname.includes('/services/unifiedServiceCenter') && (
 					<NavUnifiedServiceCenter />
 				)}

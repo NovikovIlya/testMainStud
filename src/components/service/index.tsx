a0799e7327e76ca2b1dd11f5d38954026acd0fd2
Albert Tazetdinov <zet565758@gmail.com>
2024-06-02 17:35:57 +0300

    test 3

--- src/components/service/index.tsx
+++ src/components/service/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import { useLocation } from 'react-router-dom'
+import {Navigate, useLocation, useNavigate} from 'react-router-dom'
 
 import { NavAboutMe } from './aboutMe/NavAboutMe'
 import { NavUnifiedServiceCenter } from './documentFlow/NavUnifiedServiceCenter'
@@ -13,6 +13,13 @@ import {useCheckIsEmployeeQuery} from "../../store/api/practiceApi/contracts";
 
 export const Service = () => {
 	const { pathname } = useLocation()
+	const userData = JSON.parse(localStorage.getItem('user')!)
+	const nav = useNavigate()
+
+	const {data, error, isError, isSuccess } = useCheckIsEmployeeQuery()
+
+	const isEmpl = userData.roles[1].type === 'EMPL'
+
 	return (
 		<div className="h-screen w-screen">
 			<div className="flex min-h-full">
@@ -21,7 +28,16 @@ export const Service = () => {
 				{pathname.includes('/services/aboutMe') && <NavAboutMe />}
 				{pathname.includes('/services/electronicBook') && <NavElectronicBook />}
 				{pathname.includes('/services/setting') && <NavSetting />}
-				{pathname.includes('/services/practices') && <NavPractices />}
+				{
+					isSuccess
+					&&
+					pathname.includes('/services/practices')
+						?
+						<NavPractices/>
+						:
+						<Navigate to={'/user'}/>
+				}
+
 				{pathname.includes('/services/businessTrip') && <NavBusinessTrip />}
 				{pathname.includes('/services/unifiedServiceCenter') && (
 					<NavUnifiedServiceCenter />

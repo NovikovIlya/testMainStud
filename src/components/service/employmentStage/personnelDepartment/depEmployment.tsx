2966e0bb174176180d9a77e1b3f97dfd23290ad3
Smog342 <mitro_02@mail.ru>
2024-09-03 12:12:50 +0300

    employment stage initial commit

---
+++ src/components/service/employmentStage/personnelDepartment/depEmployment.tsx
@@ -0,0 +1,9 @@
+export const DepEmployment = () => {
+	return (
+		<>
+			<div id="wrapper" className="px-[52px] mt-[120px]">
+				<p>Трудоустройство со стороны отдела кадров</p>
+			</div>
+		</>
+	)
+}

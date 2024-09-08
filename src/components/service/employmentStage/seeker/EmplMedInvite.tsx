e1885c7db977e3616a8bdfe79cf4add38727c1dc
Smog342 <mitro_02@mail.ru>
2024-09-09 00:57:55 +0300

    seeker empl stage main components initial commit

---
+++ src/components/service/employmentStage/seeker/EmplMedInvite.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+
+export const EmplMedInvite = (props: {
+	respondId: number
+	stageId: number
+}) => {
+	return (
+		<Button type="primary" className="rounded-[54.5px]">
+			Получить направление
+		</Button>
+	)
+}
--- src/components/service/employmentStage/seeker/Stages.tsx
+++ src/components/service/employmentStage/seeker/Stages.tsx
@@ -1,11 +1,22 @@
 import { useLocation } from 'react-router-dom'
 
+import { useAppSelector } from '../../../../store'
+
+import { EmplDocAttachment } from './EmplDocAttachment'
+import { EmplInstruction } from './EmplInstruction'
+import { EmplMedExam } from './EmplMedExam'
+import { EmplMedInvite } from './EmplMedInvite'
+import { EmplRequisites } from './EmplRequisites'
+import { EmplSend } from './EmplSend'
+import { EmplWorkConditions } from './EmplWorkConditions'
 import { NavPanel } from './NavPanel'
 
 export const Stages = () => {
 	const { pathname } = useLocation()
 
-	console.log(pathname.substring(pathname.lastIndexOf('/') + 1))
+	const respondId = parseInt(pathname.substring(pathname.lastIndexOf('/') + 1))
+
+	const { currentStage } = useAppSelector(state => state.currentEmploymentStage)
 
 	return (
 		<>
@@ -14,6 +25,17 @@ export const Stages = () => {
 					Вакансия «Специалист отдела сотрудничества»
 				</div>
 				<NavPanel />
+				<div className="w-full mt-[40px]">
+					{currentStage === 1 && (
+						<EmplMedInvite respondId={respondId} stageId={1} />
+					)}
+					{currentStage === 2 && <EmplDocAttachment />}
+					{currentStage === 3 && <EmplWorkConditions />}
+					{currentStage === 4 && <EmplMedExam />}
+					{currentStage === 5 && <EmplInstruction />}
+					{currentStage === 6 && <EmplRequisites />}
+					{currentStage === 7 && <EmplSend />}
+				</div>
 			</div>
 		</>
 	)

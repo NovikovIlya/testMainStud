4115a734aa07a1dc4ed32251683fca35f05718f1
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-09-09 14:35:19 +0300

    Добавил axios d список внешних зависимостей

--- vite.config.mjs
+++ vite.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
   // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
+    rollupOptions: {
+      external: ['axios']
+    }
   },
   resolve: {
     alias: {

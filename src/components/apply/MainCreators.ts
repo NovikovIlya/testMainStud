e79b41dcbf4f41c638d295cc19b4e795a95db849
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-09-09 14:54:44 +0300

    fix: ошибка с axios

--- src/components/apply/MainCreators.ts
+++ src/components/apply/MainCreators.ts
@@ -41,16 +41,16 @@ const cookies = new Cookies()
 export const getAdmission = async (dispatch: Dispatch): Promise<{link: string}| 404| 403> => {
 	try {
 		// if ((await refreshToken(dispatch)) === 403) return 403
-		const response = await getAdmissionLink()
-		debugger
-		cookies.remove('s_id', { domain: 'kpfu.ru', path: '/' })
-		cookies.remove('s_abit_id', { domain: 'kpfu.ru', path: '/' })
-		cookies.set('s_id', response.data.session, { domain: 'kpfu.ru', path: '/' })
-		cookies.set('s_abit_id', response.data.session, {
-			domain: 'kpfu.ru',
-			path: '/'
-		})
-		return response.data
+		// const response = await getAdmissionLink()
+		// debugger
+		// cookies.remove('s_id', { domain: 'kpfu.ru', path: '/' })
+		// cookies.remove('s_abit_id', { domain: 'kpfu.ru', path: '/' })
+		// cookies.set('s_id', response.data.session, { domain: 'kpfu.ru', path: '/' })
+		// cookies.set('s_abit_id', response.data.session, {
+		// 	domain: 'kpfu.ru',
+		// 	path: '/'
+		// })
+		// return response.data
 	} catch (e) {
 		return 404
 	}

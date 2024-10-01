08eea927da9b8f82cf089a05830495f98576f33f
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-10-01 15:05:06 +0300

    Ошибка sourcemap

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,6 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    sourcemap: true, 
     outDir: "build",
     rollupOptions: {
       external: ['axios']

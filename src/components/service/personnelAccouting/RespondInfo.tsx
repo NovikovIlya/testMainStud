fd35db4bc8bec36ae6b8c40b4aae0f0adea7125a
Smog342 <mitro_02@mail.ru>
2024-10-01 19:29:29 +0300

    spins fix

--- src/components/service/personnelAccouting/RespondInfo.tsx
+++ src/components/service/personnelAccouting/RespondInfo.tsx
@@ -99,8 +99,8 @@ export const RespondInfo = (props: {
 	if (res === undefined) {
 		return (
 			<>
-				<div className="w-screen h-screen flex items-center">
-					<div className="text-center ml-auto mr-auto mb-[10%]">
+				<div className="w-full h-full flex items-center">
+					<div className="text-center ml-auto mr-auto">
 						<Spin
 							indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />}
 						></Spin>

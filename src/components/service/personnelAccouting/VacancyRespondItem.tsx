16a2b874f59e29a5b9bf73a0ac249dfded81ac90
Smog342 <mitro_02@mail.ru>
2024-12-22 08:30:32 +0300

    кореектировка вёрстки у руководителя

--- src/components/service/personnelAccouting/VacancyRespondItem.tsx
+++ src/components/service/personnelAccouting/VacancyRespondItem.tsx
@@ -4,10 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 import { useLazyGetRespondFullInfoQuery } from '../../../store/api/serviceApi'
 import { setCurrentResponce } from '../../../store/reducers/CurrentResponceSlice'
-import {
-	VacancyRespondItemType,
-	respondStatus
-} from '../../../store/reducers/type'
+import { VacancyRespondItemType, respondStatus } from '../../../store/reducers/type'
 
 export const VacancyRespondItem = (
 	props: VacancyRespondItemType & {
@@ -23,24 +20,16 @@ export const VacancyRespondItem = (
 			<div className="w-full mb-[12px] flex items-center bg-white shadow-custom-shadow pl-[20px] pr-[55px] pt-[20px] pb-[20px]">
 				<p className="w-[25%]">
 					{props.userData
-						? props.userData.lastname +
-						  ' ' +
-						  props.userData.firstname +
-						  ' ' +
-						  props.userData.middlename
+						? props.userData.lastname + ' ' + props.userData.firstname + ' ' + props.userData.middlename
 						: 'Толстой Лев Николаевич'}
 				</p>
 				{props.itemType === 'PERSONNEL_DEPARTMENT' ? (
 					<>
-						<p className="ml-[10%] w-[8%]">
-							{props.responseDate.split('-').reverse().join('.')}
-						</p>
+						<p className="ml-[10%] w-[8%]">{props.responseDate.split('-').reverse().join('.')}</p>
 						<p className="ml-[1%] w-[25%]">
-							{props.status ===
-							respondStatus[respondStatus.IN_PERSONNEL_DEPT_REVIEW]
+							{props.status === respondStatus[respondStatus.IN_PERSONNEL_DEPT_REVIEW]
 								? 'на рассмотрении у отдела кадров'
-								: props.status ===
-								  respondStatus[respondStatus.IN_SUPERVISOR_REVIEW]
+								: props.status === respondStatus[respondStatus.IN_SUPERVISOR_REVIEW]
 								? 'на рассмотрении у руководителя'
 								: props.status === respondStatus[respondStatus.INVITATION]
 								? 'приглашение'
@@ -51,9 +40,18 @@ export const VacancyRespondItem = (
 					</>
 				) : (
 					<>
-						<p className="ml-[5%] w-[25%]">{props.vacancyName}</p>
-						<p className="ml-[5%] w-[8%]">
-							{props.responseDate.split('-').reverse().join('.')}
+						<p className="ml-[5%] w-[15%]">{props.vacancyName}</p>
+						<p className="ml-[5%] w-[8%]">{props.responseDate.split('-').reverse().join('.')}</p>
+						<p className="ml-[5%] w-[15%]">
+							{props.status === respondStatus[respondStatus.IN_PERSONNEL_DEPT_REVIEW]
+								? 'на рассмотрении у отдела кадров'
+								: props.status === respondStatus[respondStatus.IN_SUPERVISOR_REVIEW]
+								? 'на рассмотрении у руководителя'
+								: props.status === respondStatus[respondStatus.INVITATION]
+								? 'приглашение'
+								: props.status === respondStatus[respondStatus.REJECTED]
+								? 'отклонено'
+								: 'на рассмотрении'}
 						</p>
 					</>
 				)}
@@ -62,9 +60,7 @@ export const VacancyRespondItem = (
 						dispatch(setCurrentResponce(props.id))
 						props.itemType === 'PERSONNEL_DEPARTMENT'
 							? navigate(`services/personnelaccounting/responds/fullinfo`)
-							: navigate(
-									'services/personnelaccounting/supervisor/responds/fullinfo'
-							  )
+							: navigate('services/personnelaccounting/supervisor/responds/fullinfo')
 					}}
 					className="ml-[10%] max-w-[15%] font-content-font font-normal text-black text-[16px]/[16px] rounded-[54.5px] py-[8px] px-[24px] border-black"
 				>
--- src/components/service/personnelAccouting/supervisor/RespondsSupervisor.tsx
+++ src/components/service/personnelAccouting/supervisor/RespondsSupervisor.tsx
@@ -1,46 +1,22 @@
 import { LoadingOutlined } from '@ant-design/icons'
-import { Spin } from 'antd'
-import { useEffect, useState } from 'react'
+import { Radio, Spin } from 'antd'
+import { useState } from 'react'
 
-import {
-	useGetSupervisorRespondsQuery,
-	useLazyGetResponcesByVacancyQuery,
-	useLazyGetVacancyGroupedResponcesQuery
-} from '../../../../store/api/serviceApi'
-import { VacancyRespondItemType } from '../../../../store/reducers/type'
+import { useGetSupervisorRespondsQuery } from '../../../../store/api/serviceApi'
+import { respondStatus } from '../../../../store/reducers/type'
 import { VacancyRespondItem } from '../VacancyRespondItem'
 
 export const RespondsSupervisor = () => {
-	//const [responds, setResponds] = useState<VacancyRespondItemType[]>([])
-	// const [getGroupedResponds] = useLazyGetVacancyGroupedResponcesQuery()
-	// const [getResponds] = useLazyGetResponcesByVacancyQuery()
-
-	const { data: responds = [], isLoading: loading } =
-		useGetSupervisorRespondsQuery()
-
-	// useEffect(() => {
-	// 	getGroupedResponds({ category: 'АУП', role: 'SUPERVISOR' })
-	// 		.unwrap()
-	// 		.then(grData => {
-	// 			grData.map(vacResp => {
-	// 				getResponds({ id: vacResp.vacancyId, status: '', role: 'SUPERVISOR' })
-	// 					.unwrap()
-	// 					.then(data => setResponds(prev => [...prev, ...data]))
-	// 			})
-	// 		})
-	// }, [])
+	const [status, setStatus] = useState('все')
+	const { data: responds = [], isLoading: loading } = useGetSupervisorRespondsQuery()
 
 	if (loading) {
 		return (
 			<>
 				<div className="w-full h-full flex items-center">
 					<div className="text-center ml-auto mr-auto">
-						<Spin
-							indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />}
-						></Spin>
-						<p className="font-content-font font-normal text-black text-[18px]/[18px]">
-							Идёт загрузка...
-						</p>
+						<Spin indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />}></Spin>
+						<p className="font-content-font font-normal text-black text-[18px]/[18px]">Идёт загрузка...</p>
 					</div>
 				</div>
 			</>
@@ -50,18 +26,84 @@ export const RespondsSupervisor = () => {
 	return (
 		<>
 			<div className="pl-[54px] pr-[54px] pt-[120px] w-full bg-content-gray">
-				<h1 className="font-content-font font-normal text-[28px]/[28px] text-black">
-					Отклики
-				</h1>
+				<h1 className="font-content-font font-normal text-[28px]/[28px] text-black">Отклики</h1>
+				<div className="mt-[52px] mb-[60px] flex items-center gap-[16px]">
+					<Radio.Group
+						className="flex flex-wrap gap-[12px]"
+						value={status}
+						onChange={e => {
+							setStatus(e.target.value)
+							console.log(status)
+						}}
+					>
+						<label
+							className={`rounded-[54.5px] py-[8px] px-[16px] font-content-font ${
+								status === 'все' ? 'text-white bg-dasha-blue' : 'text-black border-solid border-black border-[1px]'
+							} font-normal text-[16px]/[16px]`}
+						>
+							<Radio value={'все'} className="hidden"></Radio>
+							все
+						</label>
+						<label
+							className={`rounded-[54.5px] py-[8px] px-[16px] font-content-font ${
+								status === respondStatus[respondStatus.IN_PERSONNEL_DEPT_REVIEW]
+									? 'text-white bg-dasha-blue'
+									: 'text-black border-solid border-black border-[1px]'
+							} font-normal text-[16px]/[16px]`}
+						>
+							<Radio value={respondStatus[respondStatus.IN_PERSONNEL_DEPT_REVIEW]} className="hidden"></Radio>
+							на рассмотрении
+						</label>
+						<label
+							className={`rounded-[54.5px] py-[8px] px-[16px] font-content-font ${
+								status === respondStatus[respondStatus.INVITATION]
+									? 'text-white bg-dasha-blue'
+									: 'text-black border-solid border-black border-[1px]'
+							} font-normal text-[16px]/[16px]`}
+						>
+							<Radio value={respondStatus[respondStatus.INVITATION]} className="hidden"></Radio>
+							приглашение на собеседование
+						</label>
+						<label
+							className={`rounded-[54.5px] py-[8px] px-[16px] font-content-font ${
+								status === respondStatus[respondStatus.EMPLOYMENT_REQUEST]
+									? 'text-white bg-dasha-blue'
+									: 'text-black border-solid border-black border-[1px]'
+							} font-normal text-[16px]/[16px]`}
+						>
+							<Radio value={respondStatus[respondStatus.EMPLOYMENT_REQUEST]} className="hidden"></Radio>
+							этап трудоустройства
+						</label>
+						<label
+							className={`rounded-[54.5px] py-[8px] px-[16px] font-content-font ${
+								status ===
+								`statuses=${respondStatus[respondStatus.IN_RESERVE]}&statuses=${respondStatus[respondStatus.ARCHIVE]}`
+									? 'text-white bg-dasha-blue'
+									: 'text-black border-solid border-black border-[1px]'
+							} font-normal text-[16px]/[16px]`}
+						>
+							<Radio
+								value={`statuses=${respondStatus[respondStatus.IN_RESERVE]}&statuses=${
+									respondStatus[respondStatus.ARCHIVE]
+								}`}
+								className="hidden"
+							></Radio>
+							отказано
+						</label>
+					</Radio.Group>
+				</div>
 				<div className="flex mt-[60px] mb-[16px] pl-[20px] pr-[55px]">
 					<h3 className="w-[25%] font-content-font text-black font-normal text-[14px]/[14px] opacity-[60%]">
 						Соискатель
 					</h3>
-					<h3 className="ml-[5%] w-[25%] font-content-font text-black font-normal text-[14px]/[14px] opacity-[60%]">
+					<h3 className="ml-[5%] w-[15%] font-content-font text-black font-normal text-[14px]/[14px] opacity-[60%]">
 						Вакансия
 					</h3>
 					<h3 className="ml-[5%] w-[8%] font-content-font text-black font-normal text-[14px]/[14px] opacity-[60%]">
 						Дата отклика
+					</h3>
+					<h3 className="ml-[5%] w-[15%] font-content-font text-black font-normal text-[14px]/[14px] opacity-[60%]">
+						Статус
 					</h3>
 				</div>
 				{responds.map(resp => (

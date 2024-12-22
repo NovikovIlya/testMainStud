abe3367fea1c6bd79649e23b99d62fedf8fafa55
Smog342 <mitro_02@mail.ru>
2024-12-22 12:55:52 +0300

    окончательные правки по руководителю

--- src/components/service/personnelAccouting/VacancyRespondItem.tsx
+++ src/components/service/personnelAccouting/VacancyRespondItem.tsx
@@ -1,4 +1,5 @@
-import { Button, ConfigProvider, Modal } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import { Button, ConfigProvider, Modal, Spin } from 'antd'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
@@ -25,6 +26,7 @@ export const VacancyRespondItem = (
 	const dispatch = useDispatch()
 
 	const { docs } = useAppSelector(state => state.employmentSeekerDocs)
+	const [empStatus, setEmpStatus] = useState<string>('')
 
 	const [getEmpData, empDataStatus] = useLazyGetEmploymentStageStatusForSupervisorQuery()
 	const [getEmpDocs, empDocs] = useLazyGetEmploymentDocsQuery()
@@ -53,9 +55,27 @@ export const VacancyRespondItem = (
 						setIsModalOpen(false)
 					}}
 				>
-					<div className="flex flex-col items-center">
-						<NavPanel />
-					</div>
+					{empDataStatus.isFetching || empDataStatus.isLoading || empDocs.isFetching || empDocs.isLoading ? (
+						<>
+							<div className="w-full h-full flex items-center">
+								<div className="text-center ml-auto mr-auto">
+									<Spin indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />}></Spin>
+									<p className="font-content-font font-normal text-black text-[18px]/[18px]">Идёт загрузка...</p>
+								</div>
+							</div>
+						</>
+					) : (
+						<div className="flex flex-col items-center">
+							<p className="font-content-font font-normal text-black text-[24px]/[28.8px] mr-auto">
+								<span className="font-bold">Этап трудоустройства: </span>
+								{props.userData?.lastname + ' ' + props.userData?.firstname + ' ' + props.userData?.middlename}
+							</p>
+							<NavPanel type="SUPERVISOR" />
+							<p className="font-content-font font-normal text-black text-[16px]/[16px] mr-auto mt-[25px]">
+								{empStatus}
+							</p>
+						</div>
+					)}
 				</Modal>
 			</ConfigProvider>
 			<div className="w-full mb-[12px] flex items-center bg-white shadow-custom-shadow pl-[20px] pr-[55px] pt-[20px] pb-[20px]">
@@ -87,6 +107,7 @@ export const VacancyRespondItem = (
 							<a
 								className="ml-[5%] w-[15%] underline text-dasha-blue underline-offset-4 cursor-pointer"
 								onClick={() => {
+									setIsModalOpen(true)
 									getEmpDocs(props.id)
 										.unwrap()
 										.then(res => {
@@ -123,9 +144,15 @@ export const VacancyRespondItem = (
 															})
 														)
 													)
-												})
-												.then(() => {
-													setIsModalOpen(true)
+													setEmpStatus(
+														empData.status === 'VERIFYING'
+															? 'Соискатель прошёл все этапы трудоустройства и отправил их на рассмотрение, ждёт подтверждения'
+															: empData.status === 'REFINE'
+															? 'Некоторые из этапов трудоустройства у соискателя были отправлены на доработку'
+															: empData.status === 'ACCEPTED'
+															? 'Соискатель успешно прошёл все этапы трудоустройства'
+															: ''
+													)
 												})
 										})
 								}}

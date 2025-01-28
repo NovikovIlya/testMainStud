8147b70080baf86fa3df1e25a561b05d3263a7cc
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-01-28 13:50:31 +0300

    fix: Исправил путь картинки

--- src/components/service/forTeachers/forTeachersService/InfoCard.tsx
+++ src/components/service/forTeachers/forTeachersService/InfoCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InfoCard = ({ text }: any) => {
 	return (
 		<div className="bg-[#FFF0E5] shadow p-4 flex gap-3">
-			<img className="w-[32px] h-[32px]" src="/public/vosk.png" />
+			<img className="w-[32px] h-[32px]" src="/vosk.png" />
 			<div>{text}</div>
 		</div>
 	)

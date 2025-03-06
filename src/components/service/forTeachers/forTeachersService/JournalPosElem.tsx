27ea1fc2fb0ed47483fcfbd89e3c26f2978ea1ce
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-03-06 14:14:01 +0300

    feat Преподавателям

--- src/components/service/forTeachers/forTeachersService/JournalPosElem.tsx
+++ src/components/service/forTeachers/forTeachersService/JournalPosElem.tsx
@@ -1,13 +1,14 @@
 import { PrinterOutlined, StarOutlined } from '@ant-design/icons'
-import { Button, Col, Form, Radio, Result, Row, Select } from 'antd'
+import { Button, Col, Form, Modal, Radio, Result, Row, Select, Spin } from 'antd'
 import { t } from 'i18next'
 import React, { useEffect, useState } from 'react'
+
+import { useAppSelector } from '../../../../store'
+import { useGetDataSemesterQuery, useGetDisciplineSemesterQuery } from '../../../../store/api/forTeacher/forTeacherApi'
 
 import InfoCard from './InfoCard'
 import TableBrs from './table/TableBrs'
 import TableJournalPos from './table/TableJournalPos'
-import { useGetDataSemesterQuery, useGetDisciplineSemesterQuery } from '../../../../store/api/forTeacher/forTeacherApi'
-import { useAppSelector } from '../../../../store'
 
 const JournalPosElem = () => {
 	const initialDay = ''
@@ -16,120 +17,148 @@ const JournalPosElem = () => {
 	const semestrForm = useAppSelector(state => state.forTeacher.semestrForm)
 	const discilineForm = Form.useWatch('disciline', form)
 	const groupeForm = Form.useWatch('group', form)
-	const [monthValue,setMonthValue] = useState(null)
-	const [disciplineId,setDisciplineId] = useState<any>(null)
-	const [groupId,setGroupId] = useState<any>(null)
-    const {data,isFetching} = useGetDisciplineSemesterQuery({year:yearForm,semester:semestrForm},{skip:!yearForm || !semestrForm})
-	const {data:dataGetSemestr} = useGetDataSemesterQuery({subjectId:disciplineId,groupId:groupId,month:monthValue,year:yearForm,semester:semestrForm},{skip:!yearForm || !semestrForm || !monthValue || !groupId })	
+	const [monthValue, setMonthValue] = useState(null)
+	const [disciplineId, setDisciplineId] = useState<any>(null)
+	const [groupId, setGroupId] = useState<any>(null)
+	const { data, isFetching } = useGetDisciplineSemesterQuery(	{ year: yearForm, semester: semestrForm },{ skip: !yearForm || !semestrForm })
+	const { data: dataGetSemestr,isFetching:isFetchingData } = useGetDataSemesterQuery({ subjectId: disciplineId, groupId: groupId, month: monthValue, year: yearForm, semester: semestrForm },{ skip: !yearForm || !semestrForm || !monthValue || !groupId })
 	const [dataSource, setDataSource] = useState<any[]>([])
+	const [checkboxValue, setCheckboxValue] = useState<any>([])
+	const [isModalOpen, setIsModalOpen] = useState(false)
 
-	useEffect(()=>{
-		if(dataGetSemestr){
+	useEffect(() => {
+		if (dataGetSemestr) {
 			setDataSource(dataGetSemestr)
 		}
-	},[dataGetSemestr])
+	}, [dataGetSemestr])
 
+	console.log('dataSource', dataSource)
+	console.log('checkboxValue', checkboxValue)
 
 	const getMonthsBySemester = (semester: any, year: any) => {
 		if (semester === 1) {
-		  return ['september', 'october', 'november', 'december']
+			return ['september', 'october', 'november', 'december']
 		}
-		
+
 		if (year === 2025) {
-		  return ['march', 'april', 'may', 'june']
+			return ['march', 'april', 'may', 'june']
 		}
-		
+
 		return ['february', 'march', 'april', 'may']
 	}
 
 	const months = getMonthsBySemester(semestrForm, yearForm)
 
-	const onChange = ()=>{
+	const onChange = () => {}
 
+	const showModal = () => {
+		setIsModalOpen(true)
+	}
+
+	const handleClose = () => {
+		setIsModalOpen(false)
+	}
+
+	const saveData = () => {
+		console.log('checkboxValue', checkboxValue)
+		const checks = checkboxValue.map((item: any) => item.columnNumber)
+		console.log('ckecks', checks)
+		console.log('dataSource', dataSource)
 	}
 
 	return (
-		<div className="">
+		<Spin spinning={isFetchingData} className="container ">
 			<Form className="mt-8" form={form}>
 				<Row>
 					<Col span={24}>
 						<Form.Item
 							name="disciline"
-							label='Предмет/Группа'
+							label="Предмет/Группа"
 							labelAlign="left"
-							labelCol={{ span: 4 }} // Фиксированная ширина лейбла
-							wrapperCol={{ span: 18 }} // Оставшаяся ширина для инпута
+							labelCol={{ span: 2 }} // Фиксированная ширина лейбла
+							wrapperCol={{ span: 10 }} // Оставшаяся ширина для инпута
 						>
 							<Select
-							    loading={isFetching}
+								loading={isFetching}
 								allowClear
-								onSelect={(value)=>{
-									console.log('22222',value)
-									console.log('3333444',Number(value.split('/')[0]))
+								onSelect={value => {
+									console.log('22222', value)
+									console.log('3333444', Number(value.split('/')[0]))
 									setDisciplineId(Number(value.split('/')[0]))
 									setGroupId(Number(value.split('/')[1]))
 								}}
-								options={
-									data?.map((item:any)=>{
-										return {
-											label:item.disciplineName + ' / ' + item.groupName,
-											value:item.disciplineId + '/' + item.groupId,
-											key:item.disciplineId + '/' + item.groupId
-										}
-									})
-								}
+								options={data?.map((item: any) => {
+									return {
+										label: item.disciplineName + ' / ' + item.groupName,
+										value: item.disciplineId + '/' + item.groupId,
+										key: item.disciplineId + '/' + item.groupId
+									}
+								})}
 							/>
 						</Form.Item>
 					</Col>
-
-
 				</Row>
 			</Form>
 
-			
-
 			{true ? (
 				<>
-				<div className={` mt-14  radio w-full justify-center animate-fade-in mb-6`}>
-				<Radio.Group
-					onChange={onChange}
-					defaultValue={initialDay}
-					buttonStyle="solid"
-					className="flex gap-[10px] h-9"
-					>
-					{months.map((month,index) => (
-						<Radio.Button
-							className="rounded-full bg-transparent h-full flex items-center text-base"
-							onChange={(value)=>{
-								setMonthValue(value.target.value)
-								console.log('value,',Number(value.target.value))}
-							}
-							value={index+1}
-							>
-							{t(month)}
-						</Radio.Button>
-					))}
-				</Radio.Group>
-				</div>
-				<div className='animate-fade-in'>
-					<Row className="flex gap-2">
-						<Button className="rounded-xl" icon={<PrinterOutlined />}>
-							{t('printJournalEmpty')}
-						</Button>
-						<Button className="rounded-xl" icon={<PrinterOutlined />}>
-							{t('printJournalFiled')}
-						</Button>
-					</Row>
-
-					<TableJournalPos setDataSource={setDataSource} dataSource={dataSource} />
-
+					<div className={` mt-10  radio w-full justify-center animate-fade-in mb-6`}>
+						<Radio.Group
+							onChange={onChange}
+							defaultValue={initialDay}
+							buttonStyle="solid"
+							className="flex gap-[10px] h-9"
+						>
+							{months.map((month, index) => (
+								<Radio.Button
+									className="rounded-full bg-transparent h-full flex items-center text-base"
+									onChange={value => {
+										setMonthValue(value.target.value)
+										console.log('value,', Number(value.target.value))
+									}}
+									value={index + 1}
+								>
+									{t(month)}
+								</Radio.Button>
+							))}
+						</Radio.Group>
+					</div>
+					<div className="animate-fade-in w-[80vw]">
+						<Row className="flex gap-2">
+							<Button className="rounded-xl" icon={<PrinterOutlined />}>
+								{t('printJournalEmpty')}
+							</Button>
+							<Button className="rounded-xl" icon={<PrinterOutlined />}>
+								{t('printJournalFiled')}
+							</Button>
+						</Row>
+						{/* <Button onClick={showModal}>Открыть журнал</Button> */}
+						{/* <Modal
+					maskClosable={false}
+					title="Журнал посещаемости"
+					open={isModalOpen}
 					
-				</div>
+					onCancel={handleClose}
+					footer={null}
+					width="100%"
+					style={{ height: 'calc(100vh - 55px)' }}
+				
+					> */}
+						<TableJournalPos
+							setCheckboxValue={setCheckboxValue}
+							setDataSource={setDataSource}
+							dataSource={dataSource}
+						/>
+						{/* </Modal> */}
+						<Button onClick={saveData} className="mt-4 mb-4 rounded-xl" type="primary">
+							{t('Save')}
+						</Button>
+					</div>
 				</>
 			) : (
 				<Result title="" extra={t('selectYearSemest')} />
 			)}
-		</div>
+		</Spin>
 	)
 }
 
--- src/components/service/forTeachers/forTeachersService/table/TableJournalPos.tsx
+++ src/components/service/forTeachers/forTeachersService/table/TableJournalPos.tsx
@@ -1,25 +1,25 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import type { FormInstance, GetRef, InputRef, TableProps } from 'antd';
-import { Button, Checkbox, Form, Input, Popconfirm, Table, Tooltip } from 'antd';
-import { t } from 'i18next';
-import { useAppDispatch } from '../../../../../store';
-import { setIsEditTableScheduleTeacher } from '../../../../../store/reducers/authSlice';
-import { ColumnTypes, DataType, EditableCellProps, EditableRowProps } from '../../../../../models/tables';
-import { truncateString } from '../../../../../utils/truncateString';
 
 
-const EditableContext = React.createContext<FormInstance<any> | null>(null);
+import React, { useContext, useEffect, useRef, useState } from "react"
+import type { FormInstance, InputRef } from "antd"
+import { Button, Checkbox, Form, Input, Select, Table, Tooltip } from "antd"
+import { t } from "i18next"
+import { useAppDispatch } from "../../../../../store"
+import { setIsEditTableScheduleTeacher } from "../../../../../store/reducers/authSlice"
+import type { EditableCellProps, EditableRowProps } from "../../../../../models/tables"
+
+const EditableContext = React.createContext<FormInstance<any> | null>(null)
 
 const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm()
   return (
     <Form form={form} component={false}>
       <EditableContext.Provider value={form}>
         <tr {...props} />
       </EditableContext.Provider>
     </Form>
-  );
-};
+  )
+}
 
 const EditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({
   title,
@@ -30,280 +30,311 @@ const EditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({
   handleSave,
   ...restProps
 }) => {
-  const [editing, setEditing] = useState(false);
-  const inputRef = useRef<InputRef>(null);
-  const form = useContext(EditableContext)!;
- 
+  const [editing, setEditing] = useState(false)
+  const inputRef = useRef<InputRef>(null)
+  const form = useContext(EditableContext)!
+  const selectRef = useRef<any>(null);
+
+
   useEffect(() => {
     if (editing) {
-      inputRef.current?.focus();
+      inputRef.current?.focus()
     }
-  }, [editing]);
+  }, [editing])
 
   const toggleEdit = () => {
-    setEditing(!editing);
-    form.setFieldsValue({ [dataIndex]: record[dataIndex] });
-  };
+    setEditing(!editing)
+    form.setFieldsValue({ [dataIndex]: record[dataIndex] || "" })
+  }
 
   const save = async () => {
     try {
-      const values = await form.validateFields();
-    
-      toggleEdit();
-      // @ts-ignore
-      handleSave({ ...record, ...values });
+      const values = await form.validateFields()
+      toggleEdit()
+      handleSave({ ...record, ...values })
     } catch (errInfo) {
-      console.log('Save failed:', errInfo);
+      console.log("Save failed:", errInfo)
     }
-  };
+  }
 
-  let childNode = children;
+  let childNode = children
 
   if (editable) {
     childNode = editing ? (
-      <Form.Item
-        style={{ margin: 0 ,width:'auto'}}
-        name={dataIndex}
-        rules={[{ required: true, message: `${title} is required.` }]}
-      >
-        <Input className='w-[35px]' ref={inputRef} onPressEnter={save} onBlur={save} />
+      <Form.Item style={{ margin: 0, width: "auto" }} name={dataIndex} rules={[{ required: false }]}>
+        {/* <Input  className="w-[35px]" ref={inputRef} onPressEnter={save} onBlur={save} /> */}
+        <Select
+    className="w-[50px]"
+    ref={selectRef}
+    onBlur={save}
+    onChange={save}
+    options={[
+      { value: null, label: '' },
+      { value: 'б', label: 'б' },
+      { value: 'н', label: 'н' }
+    ]}
+  />
       </Form.Item>
     ) : (
       <div
-        className="editable-cell-value-wrap truncate "
-        style={{  width:'auto'}}
+        className="editable-cell-value-wrap truncate"
+        style={{ width: "auto", minHeight: "22px", cursor: "pointer" }}
         onClick={toggleEdit}
       >
         {children}
       </div>
-    );
+    )
   }
 
-  return <td {...restProps}>{childNode}</td>;
-};
+  return <td {...restProps}>{childNode}</td>
+}
 
+const TableBrs = ({setCheckboxValue, dataSource, setDataSource }: any) => {
+  
 
-const TableBrs = ({dataSource, setDataSource}:any) => {
-  const tableStyle = {
-    scrollbarWidth: 'thin',
-    scrollbarColor: '#eaeaea transparent',
-    scrollbarGutter: 'stable',
-  };
-  const dispatch = useAppDispatch()
-  const semesctr = 0
-  function handleCheckboxChange(e:any,text:any)  {
-    console.log(e.target.checked,text)
+  // Добавляем стили для редактируемых ячеек
+  useEffect(() => {
+    const style = document.createElement("style")
+    style.innerHTML = `
+      .editable-cell-value-wrap {
+        padding: 5px 12px;
+        cursor: pointer;
+      }
+      .editable-row:hover .editable-cell-value-wrap {
+        border: 1px solid #d9d9d9;
+        border-radius: 4px;
+        padding: 4px 11px;
+      }
+    `
+    document.head.appendChild(style)
+    return () => {
+      document.head.removeChild(style)
+    }
+  }, [])
+
+  // Функция для группировки по датам
+  const groupByDate = (data: any[]) => {
+    const grouped: { [key: string]: any[] } = {}
+    data.forEach((student) => {
+      student.attendance.forEach((att: any) => {
+        if (!grouped[att.date]) {
+          grouped[att.date] = []
+        }
+        if (!grouped[att.date].find((x: any) => x.time === att.time && x.timeIntervalId === att.timeIntervalId)) {
+          grouped[att.date].push({
+            time: att.time,
+            timeIntervalId: att.timeIntervalId,
+            weekDay: att.weekDay,
+            columnNumber:att.columnNumber,
+          })
+        }
+      })
+    })
+    return grouped
   }
-  const defaultColumns: any = [
-    {
-        title: 'N',
-        dataIndex: 'N',
-        width: '100px',
-        fixed: 'left',
+
+  // Создаем колонки на основе сгруппированных данных
+  const generateColumns = (groupedByDate: { [key: string]: any[] }) => {
+    const baseColumns = [
+      {
+        title: "N",
+        dataIndex: "N",
+        width: "60px",
+        fixed: "left",
         customCell: () => ({
           style: {
-            backgroundColor: 'rgba(31, 92, 184, 0.2)',
-            color: 'rgba(0, 0, 0, 1)',
+            backgroundColor: "rgba(31, 92, 184, 0.2)",
+            color: "rgba(0, 0, 0, 1)",
           },
         }),
-        // @ts-ignore
-        render: (_, __, index) => index + 1 
-    },
-    {
-      title: 'ФИО',
-      dataIndex: 'studentName',
-      width: '100px',
-      fixed: 'left',
-      customCell: () => ({
-        style: {
-          backgroundColor: 'rgba(31, 92, 184, 0.2)',
-          color: 'rgba(0, 0, 0, 1)',
-        },
-      }),
-    },
-    {
-       title: 'ПН',
+        render: (_: any, __: any, index: number) => index + 1,
+      },
+      {
+        title: "ФИО",
+        dataIndex: "studentName",
+        width: "200px",
+        fixed: "left",
+        customCell: () => ({
+          style: {
+            backgroundColor: "rgba(31, 92, 184, 0.2)",
+            color: "rgba(0, 0, 0, 1)",
+          },
+        }),
+      },
+    ]
 
-      children: [
-        {
+    // Добавляем колонки для каждой даты
+    const weekDayNames = ["", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+    const dateColumns = Object.entries(groupedByDate).map(([date, times]) => {
+      // Берем день недели из первого элемента массива times
+      const weekDay = times[0]?.weekDay
+      return {
+        title: (
+          <div className="flex flex-col items-center">
+            <div className="text-[12px]">{date}</div>
+            <div className="text-[10px] text-gray-600">({weekDayNames[weekDay]})</div>
+          </div>
+        ),
+        children: times.map((timeInfo: any) => ({
           title: (
-            <div className='w-20 flex justify-center items-center flex-col'>
-               <div className='text-xs'>10:30 - 12:00</div>
-               <Tooltip title="Подтвердить?"><Checkbox onChange={(e)=>handleCheckboxChange(e,'10:30 - 12:00')} /></Tooltip>
+            <div className=" flex justify-center items-center flex-col">
+              <div className="text-[10px] text-center mb-2">{timeInfo.time}</div>
+              <Tooltip title="Подтвердить?">
+                <Checkbox onChange={(e) => handleCheckboxChange(e, { date, ...timeInfo })} />
+              </Tooltip>
             </div>
           ),
-          dataIndex: 'age2',
+          dataIndex: `status_${date}_${timeInfo.timeIntervalId}`,
+          width: "80px",
           editable: true,
-          key: 'age',
-          width: '10%',
-
-         
-          
-        },
-        
-        
-      ],
-    },
-   
-   
-  ];
-
-  const defaultColumnsTwo: (ColumnTypes[number] & { editable?: boolean; dataIndex: string })[] = [
-    {
-        title: 'N',
-        dataIndex: 'N',
-        width: '10%',
-        fixed: 'left',
-    },
-    {
-      title: 'ФИО',
-      dataIndex: 'name',
-      width: '20%',
-      fixed: 'left',
-    },
-    {
-      title: 'Февраль',
-      dataIndex: 'age',
-      editable: true,
-      width: '15%',
-      render:(text:any)=>{
-        return(
-         <div> {truncateString(2,text)}</div>
-        )
+          render: (status: string | null) => status || "",
+        })),
       }
-    },
-    {
-      title: 'Март',
-      dataIndex: 'address',
-      editable: true,
-      width: '15%',
-      render:(text:any)=>{
-        return(
-         <div> {truncateString(2,text)}</div>
-        )
+    })
+
+    return [...baseColumns, ...dateColumns]
+  }
+
+  // Преобразуем данные для таблицы
+  const transformDataForTable = (data: any[]) => {
+    return data.map((student) => {
+      const row: any = {
+        key: student.studentId,
+        studentName: student.studentName,
       }
-    },
-    {
-      title: 'Апрель',
-      dataIndex: 'address',
-      editable: true,
-      width: '15%',
-      render:(text:any)=>{
-        return(
-         <div> {truncateString(2,text)}</div>
-        )
+
+      student.attendance.forEach((att: any) => {
+        const key = `status_${att.date}_${att.timeIntervalId}`
+        row[key] = att.status || ""
+      })
+
+      return row
+    })
+  }
+
+
+  const handleCheckboxChange = (e: any, dateInfo: any) => {
+    const { checked } = e.target;
+    console.log("Checkbox changed:", checked, dateInfo);
+
+    setCheckboxValue((prev: any) => {
+      if (checked) {
+        // Если чекбокс выбран, добавляем его в массив
+        const existingIndex = prev.findIndex((item: any) => {
+          return item.date === dateInfo.date && item.time === dateInfo.time && item.timeIntervalId === dateInfo.timeIntervalId;
+        });
+
+        if (existingIndex === -1) {
+          return [...prev, { ...dateInfo, checked: checked }];
+        }
+        return prev; // Уже есть в массиве, ничего не делаем
+      } else {
+        // Если чекбокс снят, удаляем его из массива
+        return prev.filter((item: any) => {
+          return !(item.date === dateInfo.date && item.time === dateInfo.time && item.timeIntervalId === dateInfo.timeIntervalId);
+        });
       }
-    },
-    {
-      title: 'Май',
-      dataIndex: 'address',
-      editable: true,
-      width: '15%',
-      render:(text:any)=>{
-        return(
-         <div> {truncateString(2,text)}</div>
-        )
-      }
-    },
-    {
-      title: 'Сумма баллов',
-      dataIndex: 'address',
-      editable: true,
-      width: '10%',
-      render:(text:any)=>{
-        return(
-         <div> {truncateString(2,text)}</div>
-        )
-      }
-    },
-   
-  ];
-
-
-  const handleSave = (row: any) => {
-    const newData = [...dataSource];
-    // находим индекс строки
-    const index = newData.findIndex((item) => row.key === item.key);
-
-    if (index > -1) {
-      const item = newData[index];
-
-      // Сравнение старых и новых значений
-      const hasChanges = Object
-        .keys(row)
-        .some(key => item[key] !== row[key]);
-
-      // Обновление строки новыми значениями
-      newData.splice(index, 1, {
-        ...item,
-        ...row,
-      });
-
-      setDataSource(newData);
-
-      // Отправка действия, если есть изменения
-      if (hasChanges) {
-        dispatch(setIsEditTableScheduleTeacher(true));
-      }
-    }
+    });
   };
 
+  const handleSave = (row: any) => {
+    const newData = [...dataSource]
+    const index = newData.findIndex((item) => item.studentId === row.key)
+
+    if (index > -1) {
+      const student = newData[index]
+      let hasChanges = false
+
+      // Создаем новый массив attendance с обновленными значениями
+      const newAttendance = student.attendance.map((att: any) => {
+        const statusKey = `status_${att.date}_${att.timeIntervalId}`
+
+        if (row[statusKey] !== undefined && att.status !== row[statusKey]) {
+          hasChanges = true
+          // Создаем новый объект attendance с обновленным status
+          return {
+            ...att,
+            status: row[statusKey],
+          }
+        }
+
+        return att
+      })
+
+      if (hasChanges) {
+        // Создаем новый объект студента с обновленным массивом attendance
+        const updatedStudent = {
+          ...student,
+          attendance: newAttendance,
+        }
+
+        // Обновляем массив данных с новым объектом студента
+        newData[index] = updatedStudent
+        setDataSource([...newData])
+       
+      }
+    }
+  }
+
+  const groupedDates = groupByDate(dataSource)
+  const columns = generateColumns(groupedDates)
+  const transformedData = transformDataForTable(dataSource)
+
+  // Модифицируем компонент Table, чтобы сделать ячейки редактируемыми
   const components = {
     body: {
       row: EditableRow,
       cell: EditableCell,
     },
-  };
+  }
 
-  const columns = semesctr === 0 ? 
-    defaultColumns.map((col:any) => {
-    if (!col.editable) {
-      return col;
+  // Модифицируем columns для поддержки редактирования
+  const modifiedColumns = columns.map((col) => {
+    // @ts-ignore
+    if (col?.children) {
+      return {
+        ...col,
+         // @ts-ignore
+        children: col?.children.map((childCol) => {
+          if (childCol.editable) {
+            return {
+              ...childCol,
+              onCell: (record: any) => ({
+                record,
+                editable: childCol.editable,
+                dataIndex: childCol.dataIndex,
+                title: childCol.title,
+                handleSave,
+              }),
+            }
+          }
+          return childCol
+        }),
+      }
     }
-    return {
-      ...col,
-      onCell: (record: DataType) => ({
-        record,
-        editable: col.editable,
-        dataIndex: col.dataIndex,
-        title: col.title,
-        handleSave,
-      }),
-    };
-  }) 
-    :
-    defaultColumnsTwo.map((col:any) => {
-    if (!col.editable) {
-      return col;
-    }
-    return {
-      ...col,
-      onCell: (record: DataType) => ({
-        record,
-        editable: col.editable,
-        dataIndex: col.dataIndex,
-        title: col.title,
-        handleSave,
-      }),
-    };
-  });
+    return col
+  })
 
+  // В return заменяем columns на modifiedColumns
   return (
-    <div  className='mt-4 custom-table-container' >
-      <Table<DataType> 
-        rowKey={(record) => record.key}
+    <div className="mt-4 ">
+      <Table
         components={components}
-        rowClassName={() => 'editable-row'}
+        rowClassName={() => "editable-row"}
         bordered
-        dataSource={dataSource}
-        columns={columns as ColumnTypes}
+        dataSource={transformedData}
+        // @ts-ignore
+        columns={modifiedColumns}
         pagination={false}
-        
-        scroll={{ x: 'max-content' }}
+        className="w-full "
+        scroll={{ x: 'max-content', y: '50vh' }}
       />
-      <Button className='mt-4 mb-4 rounded-xl' type='primary'>{t('Save')}</Button>
+      {/* <Button className="mt-4 mb-4 rounded-xl" type="primary">
+        {t("Save")}
+      </Button> */}
     </div>
-  );
-};
+  )
+}
 
-export default TableBrs;
+export default TableBrs
+

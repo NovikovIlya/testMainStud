125f3ac93ddcf0e17e0cf29f26eb58ba597652da
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-04-03 16:55:46 +0300

    feat Обо мне

--- src/components/service/aboutMe/Languages.tsx
+++ src/components/service/aboutMe/Languages.tsx
@@ -6,26 +6,27 @@ import { Select, Space } from 'antd'
 import type { SelectProps } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
 import Title from 'antd/es/typography/Title'
+import { t } from 'i18next'
 import React, { useEffect, useState } from 'react'
 
+import { useGetNativeLanguagesQuery, useGetforeignLanguagesQuery } from '../../../store/api/aboutMe/forAboutMe'
+
+import { SkeletonPage } from './Skeleton'
 import TableLanguages from './TableLanguages'
 import UploadAvatar from './UploadAvatar'
-import { useGetforeignLanguagesQuery, useGetNativeLanguagesQuery } from '../../../store/api/aboutMe/forAboutMe'
-import { SkeletonPage } from './Skeleton'
-import { t } from 'i18next'
-
 
 const Languages = () => {
 	const [form] = Form.useForm()
 	const [isModalOpen, setIsModalOpen] = useState(false)
-	const {data:dataNative,isFetching:isFetchingNative} = useGetNativeLanguagesQuery()
-	const {data:dataForeign} = useGetforeignLanguagesQuery()
+	const [selectId, setSelectId] = useState(null)
+	const { data: dataNative, isFetching: isFetchingNative } = useGetNativeLanguagesQuery()
+	const { data: dataForeign } = useGetforeignLanguagesQuery()
 	const nativeLanguageForm = Form.useWatch('languages', form)
-	console.log('nativeLanguageForm',nativeLanguageForm)
+	console.log('selectId', selectId)
 
 	useEffect(() => {
-		form.setFieldsValue({ languages: [{value:'123',label:'123'}] }); // Устанавливает '123' как выбранный
-	}, []);
+		form.setFieldsValue({ languages: [{ value: '123', label: '123' }] }) // Устанавливает '123' как выбранный
+	}, [])
 
 	const onFinish = (values: any) => {
 		// values содержит { checkboxes: [...] }
@@ -46,139 +47,142 @@ const Languages = () => {
 	}
 
 	if (isFetchingNative)
-			return (
-				<div className="mt-[-10px] ml-[6px]">
-					<SkeletonPage />
-				</div>
-			)
+		return (
+			<div className="mt-[-10px] ml-[6px]">
+				<SkeletonPage />
+			</div>
+		)
 
 	return (
 		<div className="px-[50px] pt-[60px] mb-[50px]">
 			<div className="bg-white rounded-xl shadow-md p-[24px]">
 				<Spin spinning={false}>
-				<Row>
-					<Col span={24}>
-						<Form form={form}>
-							<Form.Item
-								label={<div className="text-[16px] font-bold">{t('nativeLanguage')}</div>} // Лейбл сверху
-								name="languages" // Ключ для данных формы
-								labelCol={{ span: 6 }}
-								wrapperCol={{ span: 12 }}
-								layout="vertical"
-							>
-								<Select
-									loading={false}
-								    showSearch
-									optionFilterProp="label"
-									mode="multiple"
-									allowClear
-									
-									options={ 
-										[
-											{value:'123',label:'123'},
-											{value:'321',label:'321'}
-										]
-									}
-								/>
-							</Form.Item>
+					<Row>
+						<Col span={24}>
+							<Form form={form}>
+								<Form.Item
+									label={<div className="text-[16px] font-bold">{t('nativeLanguage')}</div>} // Лейбл сверху
+									name="languages" // Ключ для данных формы
+									labelCol={{ span: 6 }}
+									wrapperCol={{ span: 12 }}
+									layout="vertical"
+								>
+									<Select
+										loading={false}
+										showSearch
+										optionFilterProp="label"
+										mode="multiple"
+										allowClear
+										options={[
+											{ value: '123', label: '123' },
+											{ value: '321', label: '321' }
+										]}
+									/>
+								</Form.Item>
 
-							<Button className='mt-4' type="primary" htmlType="submit">{t('save')}</Button>
-						</Form>
-					</Col>
-				</Row>
+								<Button className="mt-4" type="primary" htmlType="submit">
+									{t('save')}
+								</Button>
+							</Form>
+						</Col>
+					</Row>
 				</Spin>
 			</div>
 
 			<div className="bg-white rounded-xl shadow-md mt-4 p-[24px]">
 				<Spin spinning={false}>
-				<Row className="mb-3">
-					<Title className="!mb-0" level={5}>
-						{t('foreignLanguage')}
-					</Title>
-				</Row>
-				<Row>
-					<TableLanguages dataForeign={dataForeign}/>
-				</Row>
-				<Row className="flex justify-center mt-4">
-					<Button onClick={showModal} type="primary" style={{ marginBottom: 16 }}>
-						{t('add')}
-					</Button>
-				</Row>
+					<Row className="mb-3">
+						<Title className="!mb-0" level={5}>
+							{t('foreignLanguage')}
+						</Title>
+					</Row>
+					<Row>
+						<TableLanguages setSelectId={setSelectId} dataForeign={dataForeign} />
+					</Row>
+					<Row className="flex justify-center mt-4">
+						<Button onClick={showModal} type="primary" style={{ marginBottom: 16 }}>
+							{t('add')}
+						</Button>
+					</Row>
 				</Spin>
 
-				<Modal className='' footer={null} title={t('langZnan')} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-					<Form  className='mt-4'>
+				<Modal
+					className=""
+					footer={null}
+					title={t('langZnan')}
+					open={isModalOpen}
+					onOk={handleOk}
+					onCancel={handleCancel}
+				>
+					<Form className="mt-4">
 						<Form.Item
-								label={<div className="">{t('language')}</div>} 
-								name="languagesForm" 
-								labelCol={{ span: 6 }}
-								wrapperCol={{ span: 24 }}
-								layout="vertical"
-								className='mt-4'
-							>
-								<Select
-									mode="multiple"
-									allowClear
-								
-									options={[
-										{ value: 'en', label: 'English' },
-										{ value: 'es', label: 'Spanish' }
-									]}
-								/>
-							</Form.Item>
+							label={<div className="">{t('language')}</div>}
+							name="languagesForm"
+							labelCol={{ span: 6 }}
+							wrapperCol={{ span: 24 }}
+							layout="vertical"
+							className="mt-4"
+						>
+							<Select
+								mode="multiple"
+								allowClear
+								options={[
+									{ value: 'en', label: 'English' },
+									{ value: 'es', label: 'Spanish' }
+								]}
+							/>
+						</Form.Item>
 
+						<Form.Item
+							label={<div className="">{t('level')}</div>}
+							name="levelForm"
+							labelCol={{ span: 12 }}
+							wrapperCol={{ span: 24 }}
+							layout="vertical"
+							className="mt-14"
+						>
+							<Select
+								mode="multiple"
+								allowClear
+								options={[
+									{ value: 'en', label: 'English' },
+									{ value: 'es', label: 'Spanish' }
+								]}
+							/>
+						</Form.Item>
 
-							<Form.Item
-								label={<div className="">{t('level')}</div>} 
-								name="levelForm" 
-								labelCol={{ span: 12 }}
-								wrapperCol={{ span: 24 }}
-								layout="vertical"
-								className='mt-14'
-							>
-								<Select
-									mode="multiple"
-									allowClear
-								
-									options={[
-										{ value: 'en', label: 'English' },
-										{ value: 'es', label: 'Spanish' }
-									]}
-								/>
-							</Form.Item>
+						<Form.Item
+							label={<div className="">{t('sert')}</div>}
+							name="sertificateForm"
+							labelCol={{ span: 12 }}
+							wrapperCol={{ span: 24 }}
+							layout="vertical"
+							className="mt-14"
+						>
+							<Select
+								mode="multiple"
+								allowClear
+								options={[
+									{ value: 'en', label: 'English' },
+									{ value: 'es', label: 'Spanish' }
+								]}
+							/>
+						</Form.Item>
 
+						<div className="mt-14 mb-2">{t('prikrep')}</div>
+						<Upload className="" maxCount={1}>
+							<Button className=" " icon={<UploadOutlined />}>
+								{t('add')}
+							</Button>
+						</Upload>
 
-							<Form.Item
-								label={<div className="">{t('sert')}</div>} 
-								name="sertificateForm" 
-								labelCol={{ span: 12 }}
-								wrapperCol={{ span: 24 }}
-								layout="vertical"
-								className='mt-14'
-							>
-								<Select
-									mode="multiple"
-									allowClear
-								
-									options={[
-										{ value: 'en', label: 'English' },
-										{ value: 'es', label: 'Spanish' }
-									]}
-								/>
-							</Form.Item>
+						<Form.Item className="mt-6" name="sogl" valuePropName="checked" label={null}>
+							<Checkbox>{t('razrer')}</Checkbox>
+						</Form.Item>
 
-
-							<div className='mt-14 mb-2'>{t('prikrep')}</div>
-							<Upload className=''  maxCount={1}  >
-               					 <Button className=' ' icon={<UploadOutlined />}>{t('add')}</Button>
-             				</Upload>
-
-							 <Form.Item className='mt-6' name="sogl" valuePropName="checked" label={null}>
-							   <Checkbox>{t('razrer')}</Checkbox>
-							 </Form.Item>
-							
-
-							<Button type='primary' htmlType='submit'>{t('add')}</Button>
+						<Button type="primary" htmlType="submit">
+							{t('add')}
+						</Button>
 					</Form>
 				</Modal>
 			</div>
--- src/components/service/aboutMe/TableLanguages.tsx
+++ src/components/service/aboutMe/TableLanguages.tsx
@@ -1,8 +1,9 @@
-import { DeleteTwoTone, EditTwoTone, EyeTwoTone } from '@ant-design/icons'
-import { Button, ConfigProvider, Row, Space, Table, Tag } from 'antd'
+import { DeleteTwoTone, EditTwoTone, EyeTwoTone, UploadOutlined } from '@ant-design/icons'
+import { Button, Checkbox, ConfigProvider, Form, Modal, Row, Select, Space, Table, Tag, Upload } from 'antd'
 import type { TableProps } from 'antd'
 import { t } from 'i18next'
-import React from 'react'
+import React, { useState } from 'react'
+
 import { truncateString } from '../../../utils/truncateString'
 
 interface DataType {
@@ -13,11 +14,9 @@ interface DataType {
 	tags: string[]
 }
 
+const TableLanguages = ({ dataForeign, setSelectId }: any) => {
+	const [isModalOpenEdit, setIsModalOpenEdit] = useState(false)
 
-
-
-
-const TableLanguages = ({dataForeign}:any) => {
 	const columns: TableProps<DataType>['columns'] = [
 		{
 			title: t('language'),
@@ -34,21 +33,19 @@ const TableLanguages = ({dataForeign}:any) => {
 			title: t('sert'),
 			dataIndex: 'certificates',
 			key: 'address',
-			render: (certificates) => (
-			  <>
-				{certificates?.map((item:any, index:any) => (
-				  <div key={index}>
-					<a target="_blank" href={item.certificateLink} rel="noopener noreferrer">
-					  {item.certificateName} {index===certificates.length-1 ? '' : ', '}
-					</a>
-				  </div>
-				))}
-			  </>
-			),
-		  
-		  
+			render: certificates => (
+				<>
+					{certificates?.map((item: any, index: any) => (
+						<div key={index}>
+							<a target="_blank" href={item.certificateLink} rel="noopener noreferrer">
+								{item.certificateName} {index === certificates.length - 1 ? '' : ', '}
+							</a>
+						</div>
+					))}
+				</>
+			)
 		},
-	
+
 		{
 			title: '',
 			key: 'action',
@@ -63,7 +60,15 @@ const TableLanguages = ({dataForeign}:any) => {
 			key: 'action',
 			render: (_, record) => (
 				<Space size="middle">
-					<EditTwoTone />
+					<EditTwoTone
+						className='hover:scale-[140%] '
+						onClick={() => {
+							console.log('record', record)
+							// @ts-ignore
+							setSelectId(record?.studLangId)
+							showModalEdit()
+						}}
+					/>
 				</Space>
 			)
 		},
@@ -78,20 +83,18 @@ const TableLanguages = ({dataForeign}:any) => {
 		}
 	]
 
-	const data:any = [
+	const data: any = [
 		{
 			key: '1',
 			name: 'John Brown',
 			age: 32,
 			address: 'New York No. 1 Lake Park'
-	
 		},
 		{
 			key: '2',
 			name: 'Jim Green',
 			age: 42,
-			address: 'London No. 1 Lake Park',
-		
+			address: 'London No. 1 Lake Park'
 		},
 		{
 			key: '3',
@@ -101,6 +104,17 @@ const TableLanguages = ({dataForeign}:any) => {
 			tags: ['cool', 'teacher']
 		}
 	]
+	const showModalEdit = () => {
+		setIsModalOpenEdit(true)
+	}
+
+	const handleOkEdit = () => {
+		setIsModalOpenEdit(false)
+	}
+
+	const handleCancelEdit = () => {
+		setIsModalOpenEdit(false)
+	}
 
 	return (
 		<>
@@ -113,11 +127,90 @@ const TableLanguages = ({dataForeign}:any) => {
 					}
 				}}
 			>
-				<Table<DataType> 
-					pagination={false} 
-					columns={columns} 
-					dataSource={dataForeign} 
-					className="w-full" 
+				<Modal
+					className=""
+					footer={null}
+					title={'Редактирование'}
+					open={isModalOpenEdit}
+					onOk={handleOkEdit}
+					onCancel={handleCancelEdit}
+				>
+					<Form className="mt-4">
+						<Form.Item
+							label={<div className="">{t('language')}</div>}
+							name="languagesForm"
+							labelCol={{ span: 6 }}
+							wrapperCol={{ span: 24 }}
+							layout="vertical"
+							className="mt-4"
+						>
+							<Select
+								mode="multiple"
+								allowClear
+								options={[
+									{ value: 'en', label: 'English' },
+									{ value: 'es', label: 'Spanish' }
+								]}
+							/>
+						</Form.Item>
+
+						<Form.Item
+							label={<div className="">{t('level')}</div>}
+							name="levelForm"
+							labelCol={{ span: 12 }}
+							wrapperCol={{ span: 24 }}
+							layout="vertical"
+							className="mt-14"
+						>
+							<Select
+								mode="multiple"
+								allowClear
+								options={[
+									{ value: 'en', label: 'English' },
+									{ value: 'es', label: 'Spanish' }
+								]}
+							/>
+						</Form.Item>
+
+						<Form.Item
+							label={<div className="">{t('sert')}</div>}
+							name="sertificateForm"
+							labelCol={{ span: 12 }}
+							wrapperCol={{ span: 24 }}
+							layout="vertical"
+							className="mt-14"
+						>
+							<Select
+								mode="multiple"
+								allowClear
+								options={[
+									{ value: 'en', label: 'English' },
+									{ value: 'es', label: 'Spanish' }
+								]}
+							/>
+						</Form.Item>
+
+						<div className="mt-14 mb-2">{t('prikrep')}</div>
+						<Upload className="" maxCount={1}>
+							<Button className=" " icon={<UploadOutlined />}>
+								{t('add')}
+							</Button>
+						</Upload>
+
+						<Form.Item className="mt-6" name="sogl" valuePropName="checked" label={null}>
+							<Checkbox>{t('razrer')}</Checkbox>
+						</Form.Item>
+
+						<Button type="primary" htmlType="submit">
+							{t('add')}
+						</Button>
+					</Form>
+				</Modal>
+				<Table<DataType>
+					pagination={false}
+					columns={columns}
+					dataSource={dataForeign}
+					className="w-full"
 					locale={{ emptyText: t('noData') }}
 				/>
 			</ConfigProvider>

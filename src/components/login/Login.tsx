01319d2d71b18364d5b9061677890bc082b37ba9
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-04-24 16:19:05 +0300

    fix: isMobileLogin

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -111,7 +111,7 @@ export const Login = () => {
 				<Form
 					form={form}
 					name="login"
-					className={`min-w-[400px] rounded-lg shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] p-6 mb-4 mx-2 ${isMobile ? 'w-[90%]' : 'w-[50%]'} `}
+					className={`min-w-[400px] rounded-lg shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] p-6 mb-4 mx-2 ${isMobile ? 'w-[50%]' : ''} `}
 					initialValues={{ remember: true }}
 					onFinish={onFinish}
 				>

0a1575f0b51ad1e1ea5d28237f9e12296b1cba20
Smog342 <mitro_02@mail.ru>
2025-04-26 14:53:30 +0300

    некоторые соображения по поводу адаптива

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ErrorBoundary } from 'react-error-boundary'
 import { useTranslation } from 'react-i18next'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+import { AdaptiveHandler } from './components/AdaptiveHandler'
 import ErrorFallback from './components/ErrorFallback'
 import { NotFound } from './components/NotFound'
 import { RequireAuth } from './components/RequireAuth'
@@ -23,10 +24,10 @@ import { Notification } from './components/notification/Notification'
 import { Redirect } from './components/redirect/Redirect'
 import { Registration } from './components/registration/Registration'
 import { Service } from './components/service'
+import LandingPage from './components/service/LandingPage'
 import ShortLink from './components/service/ShortLink/ShortLink'
 import EditSchedule from './components/service/practices/forming-schedule/EditSchedule'
 import { blue004, blue307 } from './utils/color'
-import LandingPage from './components/service/LandingPage'
 
 const App = () => {
 	const [email, changeEmail] = useState('')
@@ -34,50 +35,55 @@ const App = () => {
 	const { i18n } = useTranslation()
 	const router = createBrowserRouter([
 		{
-			path: 'redirect/:id',
-			element: <Redirect />
-		},
-		{
-			path: 'registration',
-			element: <Registration email={email} changeEmail={changeEmail} />
-		},
-		{
-			path: 'registration/checkingEmail',
-			element: <CheckEmail email={email} />
-		},
-		{
-			path: 'api/register/approve/*',
-			element: <ApproveEmail />
-		},
-		{
 			path: '/',
-			element: <Login />,
+			element: <AdaptiveHandler />,
 			children: [
-				// { path: "redirect/:id", element: <Redirect /> },
-				// { path: "registration", element: <Registration email={email} changeEmail={changeEmail} /> },
-				// { path: "registration/checkingEmail", element: <CheckEmail email={email} /> },
-				// { path: "api/register/approve/*", element: <ApproveEmail /> },
-			]
-		},
-		{
-			element: <RequireAuth />,
-			errorElement: <ErrorFallback />,
-			children: [
-				{ path: 'user/*', element: <User /> },
-				{ path: 'infoUser', element: <InfoUser /> },
-				{ path: 'infoUserUpdate', element: <InfoUserUpdate /> },
-				{ path: 'form', element: <FormModal /> },
-				{ path: 'education', element: <EducationForm /> },
-				{ path: 'documents', element: <DocumentForm /> },
-				{ path: 'work', element: <WorkForm /> },
-				{ path: 'parent', element: <ParentForm /> },
-				{ path: 'services/*', element: <Service /> },
-				{ path: '*', element: <NotFound /> },
-				{ path: 'services/shorturl', element: <ShortLink /> },
-				{ path: 'landing', element: <LandingPage /> }
+				{
+					path: 'redirect/:id',
+					element: <Redirect />
+				},
+				{
+					path: 'registration',
+					element: <Registration email={email} changeEmail={changeEmail} />
+				},
+				{
+					path: 'registration/checkingEmail',
+					element: <CheckEmail email={email} />
+				},
+				{
+					path: 'api/register/approve/*',
+					element: <ApproveEmail />
+				},
+				{
+					path: '/',
+					element: <Login />,
+					children: [
+						// { path: "redirect/:id", element: <Redirect /> },
+						// { path: "registration", element: <Registration email={email} changeEmail={changeEmail} /> },
+						// { path: "registration/checkingEmail", element: <CheckEmail email={email} /> },
+						// { path: "api/register/approve/*", element: <ApproveEmail /> },
+					]
+				},
+				{
+					element: <RequireAuth />,
+					errorElement: <ErrorFallback />,
+					children: [
+						{ path: 'user/*', element: <User /> },
+						{ path: 'infoUser', element: <InfoUser /> },
+						{ path: 'infoUserUpdate', element: <InfoUserUpdate /> },
+						{ path: 'form', element: <FormModal /> },
+						{ path: 'education', element: <EducationForm /> },
+						{ path: 'documents', element: <DocumentForm /> },
+						{ path: 'work', element: <WorkForm /> },
+						{ path: 'parent', element: <ParentForm /> },
+						{ path: 'services/*', element: <Service /> },
+						{ path: '*', element: <NotFound /> },
+						{ path: 'services/shorturl', element: <ShortLink /> },
+						{ path: 'landing', element: <LandingPage /> }
 
-
-				// { path: "services/practices/formingSchedule/edit/:id", element: <EditSchedule /> },
+						// { path: "services/practices/formingSchedule/edit/:id", element: <EditSchedule /> },
+					]
+				}
 			]
 		}
 	])

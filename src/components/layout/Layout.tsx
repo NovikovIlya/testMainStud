beba4cda4eca2bf61aef25ee6d970c089fff158c
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-04-30 10:33:29 +0300

    fix - yandex2

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,25 +6,25 @@ import { ILayoutProps } from '../../models/layout'
 import CookieConsent from '../dnd/CookieConsent'
 import { useLocation } from 'react-router-dom'
 
-const YandexMetrikaTracker = () => {
-  const location = useLocation();
+// const YandexMetrikaTracker = () => {
+//   const location = useLocation();
 
-  useEffect(() => {
-	// Проверка инициализации объекта ym
-	// @ts-ignore
-	if (typeof window.ym === 'function') {
-		// @ts-ignore
-	  window.ym(100713417, 'hit', location.pathname + location.search);
-	}
-  }, [location]);
+//   useEffect(() => {
+// 	// Проверка инициализации объекта ym
+// 	// @ts-ignore
+// 	if (typeof window.ym === 'function') {
+// 		// @ts-ignore
+// 	  window.ym(100713417, 'hit', location.pathname + location.search);
+// 	}
+//   }, [location]);
 
-  return null;
-};
+//   return null;
+// };
 
 export const Layout: FC<ILayoutProps> = ({ children }) => {
 	return (
 		<div className="flex flex-col w-full bg-[#F5F8FB]">
-			<YandexMetrikaTracker />
+			{/* <YandexMetrikaTracker /> */}
 			<div className=" flex w-full h-full min-h-screen">
 				<div className="flex flex-col w-full">
 					<Header />

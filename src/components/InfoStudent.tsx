e288ad6069b66c9650adbe14d8faa7c59f673580
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-05-07 09:15:57 +0300

    fix форма

--- src/components/InfoStudent.tsx
+++ src/components/InfoStudent.tsx
@@ -13,7 +13,8 @@ const InfoStudent = () => {
 			</div>
 			<div> 	{t('welcome_message_student2')}  <a  href={`mailto:${t('welcome_message_studentMail')}`}>{t('welcome_message_studentMail')}</a></div>
 			<div>
-			<div>{t('welcome_message_student3')}  <a rel="noopener noreferrer" target="_blank"  href='https://forms.yandex.ru/cloud/681ae464505690c4ba036f1b'>{t('welcome_message_student3Form')}</a></div>
+			{/* <div>{t('welcome_message_student3')}  <a rel="noopener noreferrer" target="_blank"  href='https://forms.yandex.ru/cloud/681ae464505690c4ba036f1b'>{t('welcome_message_student3Form')}</a></div> */}
+			<div>{t('welcome_message_student3')}  <a rel="noopener noreferrer" target="_blank"  href='https://forms.yandex.ru/cloud/681ae464505690c4ba036f1b'>{t('welcome_message_student3Form')}!</a></div>
 			</div>
 			<div> 	<div>{t('welcome_message_student4')}</div> </div>
 			

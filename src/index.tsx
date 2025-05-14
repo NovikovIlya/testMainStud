05f16cd3832816e9fd211532355a4eb6d29a8add
Smog342 <mitro_02@mail.ru>
2025-05-14 14:09:48 +0300

    убрал предыдущую реализацию алертов

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import App from './App'
 import CookieConsent from './components/dnd/CookieConsent'
 import './index.scss'
 import { store } from './store'
-import { AlertProvider } from './utils/Alert/AlertMessage'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -27,10 +26,8 @@ root.render(
 		}
 	>
 		<Provider store={store}>
-			<AlertProvider>
-				<App />
-				<CookieConsent />
-			</AlertProvider>
+			<App />
+			<CookieConsent />
 		</Provider>
 	</Suspense>
 	//</React.StrictMode>

e2e91536f65ddc8d90ba708347c1679228cebb69
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-05-16 21:52:56 +0300

    refactor: update translations for email verification and contact data block
    This commit updates translations in both English and Russian locales and modifies the ContactDataBlock component.
    
    Changes include:
    
    - Updated the "privateMail" translation to "Email" in both English and Russian locales.
    - Modified the "verText22" translation in both locales to provide clearer instructions for phone number verification, specifying to enter the last 4 digits of the calling number.
    - Added a Popconfirm component for email deletion in ContactDataBlock.tsx to confirm the deletion action.

--- src/components/service/setting/Contacts/ContactDataBlock.tsx
+++ src/components/service/setting/Contacts/ContactDataBlock.tsx
@@ -1,26 +1,25 @@
 import { DeleteOutlined, PlusCircleOutlined } from '@ant-design/icons'
-import { Button, Form, Input, Select, Spin } from 'antd'
+import { Button, Form, Input, Popconfirm, Select, Spin } from 'antd'
 import { useTranslation } from 'react-i18next'
 
-
-const ContactDataBlock = ({isLoadingPost, sortedEmails, sendVer, handleDeleteEmail, showModal }: any) => {
+const ContactDataBlock = ({ isLoadingPost, sortedEmails, sendVer, handleDeleteEmail, showModal }: any) => {
 	const { t } = useTranslation()
-    
+
 	return (
 		<Spin spinning={isLoadingPost}>
-            <div className="">
-			<div className=" mx-auto bg-white rounded-xl shadow-md overflow-hidden">
-				<div className="p-6">
-					<h1 className='mb-4'>{t('privateMail')}:</h1>
-					{/* Список почт */}
-					<div className="space-y-3 mb-6">
-						{sortedEmails?.map((email: any, index: number) => (
-							<div
-								key={email.id || index}
-								className="flex items-center justify-between p-3 bg-gray-50 rounded-lg group hover:bg-gray-100 transition-colors"
-							>
-								<span className="text-gray-700">{email.email}</span>
-								{/* <Select
+			<div className="">
+				<div className=" mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+					<div className="p-6">
+						<h1 className="mb-4">{t('privateMail')}:</h1>
+						{/* Список почт */}
+						<div className="space-y-3 mb-6">
+							{sortedEmails?.map((email: any, index: number) => (
+								<div
+									key={email.id || index}
+									className="flex items-center justify-between p-3 bg-gray-50 rounded-lg group hover:bg-gray-100 transition-colors"
+								>
+									<span className="text-gray-700">{email.email}</span>
+									{/* <Select
 									defaultValue="lucy"
 									className='w-[30%]'
 									
@@ -32,46 +31,55 @@ const ContactDataBlock = ({isLoadingPost, sortedEmails, sendVer, handleDeleteEma
 									{ value: 'disabled', label: 'SMS'},
 									]}
 								/> */}
-								<div className="flex gap-3">
-									<span className="text-gray-400">
-										{email.verified ? (
-											t('verification')
-										) : (
-											<div className="cursor-pointer shadow-sm" onClick={() => sendVer(email.id)}>
-												{t('requiredVerification')}
-											</div>
-										)}
-									</span>
-									<button
-										type="button"
-										onClick={() => handleDeleteEmail(email.id)}
-										className="text-gray-400 hover:text-red-500 transition-colors border-none"
-										aria-label="Удалить"
-									>
-										<DeleteOutlined className="" />
-									</button>
+									<div className="flex gap-3">
+										<span className="text-gray-400">
+											{email.verified ? (
+												t('verification')
+											) : (
+												<div className="cursor-pointer shadow-sm" onClick={() => sendVer(email.id)}>
+													{t('requiredVerification')}
+												</div>
+											)}
+										</span>
+										<Popconfirm
+											title={t('deleteEducationTitle')}
+											description={t('deleteEducationDescription')}
+											onConfirm={() => {
+												handleDeleteEmail(email.id)
+											}}
+										>
+											<button
+											type="button"
+											// onClick={() => handleDeleteEmail(email.id)}
+											className="text-gray-400 hover:text-red-500 transition-colors border-none"
+											aria-label="Удалить"
+										>
+											<DeleteOutlined className="" />
+										</button>
+										</Popconfirm>
+										
+									</div>
 								</div>
-							</div>
-						))}
-					</div>
+							))}
+						</div>
 
-					{/* Форма добавления */}
-					<div className="flex gap-2">
-						<Form.Item
-							name={'inputText'}
-							className="p-0 w-full"
-							rules={[
-								// { required: true, message: 'Пожалуйста, введите электронную почту!' },
-								{ type: 'email', message: 'Введите корректный адрес электронной почты!' }
-							]}
-						>
-							<Input
-								type="email"
-								placeholder={t('putYourEmail')}
-								className="flex-1 rounded-lg border-gray-300 shadow-sm px-4 py-2 bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
-							/>
-						</Form.Item>
-						{/* <Form.Item
+						{/* Форма добавления */}
+						<div className="flex gap-2">
+							<Form.Item
+								name={'inputText'}
+								className="p-0 w-full"
+								rules={[
+									// { required: true, message: 'Пожалуйста, введите электронную почту!' },
+									{ type: 'email', message: 'Введите корректный адрес электронной почты!' }
+								]}
+							>
+								<Input
+									type="email"
+									placeholder={t('putYourEmail')}
+									className="flex-1 rounded-lg border-gray-300 shadow-sm px-4 py-2 bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
+								/>
+							</Form.Item>
+							{/* <Form.Item
 							name={'inputType'}
 							className=""
 							rules={[
@@ -91,19 +99,19 @@ const ContactDataBlock = ({isLoadingPost, sortedEmails, sendVer, handleDeleteEma
 									]}
 								/>
 								</Form.Item> */}
-						<Button
-							// onClick={handleAddEmail}
-							htmlType="submit"
-							className="h-[40px] flex items-center"
-						>
-							<PlusCircleOutlined className=" " />
-							<span>{t('addBtn')}</span>
-						</Button>
+							<Button
+								// onClick={handleAddEmail}
+								htmlType="submit"
+								className="h-[40px] flex items-center"
+							>
+								<PlusCircleOutlined className=" " />
+								<span>{t('addBtn')}</span>
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-        </Spin>
+		</Spin>
 	)
 }
 

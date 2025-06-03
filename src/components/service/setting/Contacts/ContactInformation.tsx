696643c8ca78231cb576d3f94630e110d90a2d1b
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-06-03 16:15:25 +0300

    feat: Implement social network input and enhance contact information UI
    This commit introduces the ability for users to input their social network links and enhances the contact information UI with a tooltip.
    
    The changes include:
    
    - Adding a TextAreaComponent for social network input (Vk.com, Telegram, Rutube, Odnoklassniki, Yandex Zen) with a maximum length of 1000 characters for each field.
    - Adding a tooltip to the ContactInformation component to provide additional information.
    - Adding loading spinner to the TextAreaComponent during data updates.

--- src/components/service/setting/Contacts/ContactInformation.tsx
+++ src/components/service/setting/Contacts/ContactInformation.tsx
@@ -1,6 +1,5 @@
 // import { Button, Card, Form, Input, Modal, Row, Spin } from 'antd'
 // import { useEffect, useState } from 'react'
-
 // import {
 // 	useDeleteAccMutation,
 // 	useFinalVerifyMutation,
@@ -14,7 +13,6 @@
 // import { showNotification } from '../../../../store/reducers/notificationSlice'
 // import { t } from 'i18next'
 // import Title from 'antd/es/typography/Title'
-
 // export const ContactInformation = () => {
 // 	const [form] = Form.useForm()
 // 	const { data: dataEmail,isError } = useGetEmailQuery()
@@ -27,14 +25,11 @@
 // 	const [count, setCount] = useState(0);
 // 	const dispatch = useAppDispatch()
 // 	const mail = useAppSelector((state)=>state.auth.user?.email)
-
-
 // 	useUpdateEffect(()=>{
 // 		if(dataVer){
 // 			setCount(dataVer.cooldownMs / 1000)
 // 		}
 // 	},[dataVer])
-
 // 	useEffect(()=>{
 // 		if(isErrorPost){
 // 			dispatch(showNotification({
@@ -43,13 +38,11 @@
 // 			}))
 // 		}
 // 	},[isErrorPost])
-
-// 	useInterval(() => { 
+// 	useInterval(() => {
 // 		setCount(count - 1);
 // 		// Интервал будет работать только когда модальное окно открыто и count > 0, где 1000 - это мс
 // 		}, isModalOpen && (count > 0) ? 1000 : undefined
 // 	);
-
 // 	const onSubmitPhone2 = () => {
 // 		if(dataEmail && dataEmail?.length>10){
 // 			alert('Нельзя добавлять больше 10 почт.')
@@ -63,61 +56,46 @@
 // 		postMail({ email: text }).unwrap().then(res => sendVerOne(res.id))
 // 		form.setFieldValue('inputText', '')
 // 	}
-
 // 	const handleDeleteEmail = (id: any) => {
 // 		deleteMail(id)
 // 	}
-
 // 	const showModal = () => {
 // 		setIsModalOpen(true)
 // 	}
-
-
 // 	const handleCancel = () => {
 // 		setIsModalOpen(false)
 // 		form.setFieldValue('code', '')
-// 		setCount(0); 
+// 		setCount(0);
 // 		dispatch(apiSlice.util.resetApiState())
 // 	}
-
 // 	const sendVer = (id: any) => {
 // 		showModal()
 // 		setIdUser(id)
 // 		// отправить код
 // 		sendVerify(id)
 // 	}
-
 // 	const sendVerOne = (id: any) => {
 // 		showModal()
 // 		setIdUser(id)
 // 		setCount(60)
-
-	
 // 	}
-	
 // 	const finalFnVer = () => {
 // 		const code = form.getFieldValue('code')
-
 // 		if(!code){
 // 			alert('Введите код')
 // 		}
-		
 // 		finalVerify({
 // 			id: idUser,
 // 			otp: code
 // 		})
 // 	}
-
 // 	// @ts-ignore
 // 	const sortedEmails = dataEmail?.toSorted((a:any, b:any) => {
 // 		return b.verified === a.verified ? 0 : b.verified ? 1 : -1
 // 	})
-
 // 	if (isError) return <></>
 // 	if (dataEmail === undefined ) return <div className='mt-[-45px] ml-[-40px]'><SkeletonPage /></div>
-
 // 	return (
-	
 // 		<Form form={form} onFinish={onSubmitPhone2} className='w-full'>
 // 			<Row className="mb-8 flex items-center justify-between mt-6">
 // 			<Title  className='!mb-0 !text-[28px]'>{t('contactInformation')}</Title></Row>
@@ -125,7 +103,7 @@
 // 				<Card className='bg-white rounded-xl shadow-md overflow-hidden mt-4 w-full'>
 // 					<h3>{t('mainMail')}</h3>
 // 					<div className='bg-gray-50 rounded-lg mt-2 p-3'>
-// 						<a href={`mailto:${mail}`}>{mail}</a>		
+// 						<a href={`mailto:${mail}`}>{mail}</a>
 // 					</div>
 // 				</Card>
 // 				<article className=" mt-10">
@@ -138,28 +116,21 @@
 // 					{count <= 0 ? <div>{!isLoadingVer ? t('verText3') : t('loading')}</div>:
 // 					<>
 // 					{dataFin?.verified ? <div className='text-center'>{t('verSuc')}</div>:<>
-// 					<p>{t('verText')}: {count}</p> 
+// 					<p>{t('verText')}: {count}</p>
 // 					<p className="mb-2">{t('verText2')}</p>
 // 					<Form.Item name={'code'} >
 // 						<Input.OTP length={4}  />
 // 					</Form.Item>
 // 					<Button className="w-full" onClick={finalFnVer}>
 // 						{t('addBtn')}
-// 					</Button></>} 
+// 					</Button></>}
 // 					</>}
 // 				</Spin>
 // 				}
 // 			</Modal>
 // 		</Form>
-
 // 	)
 // }
-
-
-
-
-
-
 import { useInterval, useUpdateEffect } from 'ahooks'
 import { Button, Card, Form, Input, Modal, Row, Spin, Tooltip } from 'antd'
 import Title from 'antd/es/typography/Title'
@@ -186,14 +157,12 @@ export const ContactInformation = () => {
 	const { data: dataEmail, isError } = useGetEmailQuery()
 	const [postMail, { data: dataPost, isLoading: isLoadingPost, isError: isErrorPost }] = usePostEmailMutation()
 	const [sendVerify, { data: dataVer, isLoading: isLoadingVer, isError: isErrorVerif }] = useVerifyAccMutation()
-	
 	const [finalVerify, { data: dataFin, isLoading: isLoadingFin }] = useFinalVerifyMutation()
 	const [deleteMail] = useDeleteAccMutation()
 	const [isModalOpen, setIsModalOpen] = useState(false)
 	const [idUser, setIdUser] = useState(null)
 	const [count, setCount] = useState(0)
 	const dispatch = useAppDispatch()
-	const mail = useAppSelector(state => state.auth.user?.email)
 	const textForm = Form.useWatch('inputText', form)
 
 	useUpdateEffect(() => {
@@ -294,18 +263,21 @@ export const ContactInformation = () => {
 	return (
 		<Form form={form} onFinish={onSubmitPhone2} className="w-full">
 			<Row className="mb-8 flex items-center justify-between mt-6">
-				<Title className="!mb-0 !text-[28px]">{t('contactInformation')}	
-						
-					</Title>
-				<Tooltip title={
-					<>
-					<div className='!text-yellow-50'>{t('textForContactInformation')}</div>
-					<div className='!text-yellow-50 font-extrabold'>123 <img className='rounded-[50%]' src='https://i.postimg.cc/13gRWSd6/image.png'/></div>
-					<div className='!text-yellow-50 text-[30px]'>55</div>
-					</>
-					}>
-						<img className=' ' src="/GroupVop.svg" />
-					
+				<Title className="!mb-0 !text-[28px]">{t('contactInformation')}</Title>
+				<Tooltip
+				    color='white'
+					title={
+						<>
+							<div  className="text-black p-2">{t('textForContactInformation')} </div>
+							<img className="" src="https://i.postimg.cc/Zq0bZ8Yv/image.png" />
+							{/* <div className="!text-yellow-50 font-extrabold">
+								123 <img className="rounded-[50%]" src="https://i.postimg.cc/Zq0bZ8Yv/image.png" />
+							</div>
+							<div className="!text-yellow-50 text-[30px]">55</div> */}
+						</>
+					}
+				>
+					<img className=" " src="/GroupVop.svg" />
 				</Tooltip>
 			</Row>
 			<section className=" w-full">
@@ -336,7 +308,7 @@ export const ContactInformation = () => {
 						) : (
 							<>
 								{dataFin?.verified ? (
-									<div className="text-center">{t('verSuc')}</div>
+									<div className="text-center ">{t('verSuc')}</div>
 								) : (
 									<>
 										<p>
--- src/components/service/setting/Contacts/TextAreaComponent.tsx
+++ src/components/service/setting/Contacts/TextAreaComponent.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input } from 'antd'
+import { Button, Form, Input, Spin } from 'antd'
 import { t } from 'i18next'
 
 import React, { useEffect } from 'react'
@@ -37,33 +37,34 @@ const TextAreaComponent = () => {
 
 	return (
 		<Form form={form} onFinish={onSubmit}>
+      <Spin spinning={isLoading || isUpdating}>
 			<section className=" mx-auto bg-white rounded-xl shadow-md overflow-hidden">
 				<div className="p-6">
 					<div className="mb-4 !text-[16px] font-bold">Соц сети:</div>
 
           <div className="mb-4 !text-[16px] ">Vk.com:</div>
 					<Form.Item name="vk" noStyle>
-						<TextArea rows={3} className='mb-8'/>
+						<TextArea maxLength={1000} rows={3} className='mb-8'/>
 					</Form.Item>
 
           <div className="mb-4 !text-[16px] ">Telegram</div>
 					<Form.Item name="telegram" noStyle>
-						<TextArea rows={3} className='mb-8'/>
+						<TextArea maxLength={1000}  rows={3} className='mb-8'/>
 					</Form.Item>
 
           <div className="mb-4 !text-[16px] ">Rutube</div>
 					<Form.Item name="rutube" noStyle>
-						<TextArea rows={3} className='mb-8'/>
+						<TextArea maxLength={1000}  rows={3} className='mb-8'/>
 					</Form.Item>
 
           <div className="mb-4 !text-[16px] ">Однолкассники</div>
 					<Form.Item name="odnoklassniki" noStyle>
-						<TextArea rows={3} className='mb-8'/>
+						<TextArea maxLength={1000}  rows={3} className='mb-8'/>
 					</Form.Item>
 
             <div className="mb-4 !text-[16px] ">Яндекс Дзен</div>
 					<Form.Item name="dzen" noStyle>
-						<TextArea rows={3} className='mb-8'/>
+						<TextArea maxLength={1000}  rows={3} className='mb-8'/>
 					</Form.Item>
 
           <Button  type="primary" htmlType="submit" className="rounded-lg" loading={isUpdating}>
@@ -71,6 +72,7 @@ const TextAreaComponent = () => {
 					</Button>
 				</div>
 			</section>
+      </Spin>
 		</Form>
 	)
 }

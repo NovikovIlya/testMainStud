e9a27ff297e06a9c7f722ce1d6f4ed1d81d8be41
NovikovIlya <novikov.i.sergeevich@gmail.com>
2025-06-27 16:44:26 +0300

    style(AccessibilityHelper): adjust bottom sheet positioning
    - Update right positioning from right-2 to right-[28px] for better alignment

--- src/components/AccessibilityHelper/AccessibilityHelper.tsx
+++ src/components/AccessibilityHelper/AccessibilityHelper.tsx
@@ -125,7 +125,7 @@ const AccessibilityHelper: React.FC<AccessibilityHelperProps> = forwardRef(({ la
         <KeyOutlined className="h-6 w-6" />
       </button> */}
       {isOpen && (
-        <div ref={ref} className="absolute bottom-12 right-2 w-74 bg-white shadow-lg rounded-lg p-8 accesibility-helper min-w-[312px]">
+        <div ref={ref} className="absolute bottom-12 right-[28px] w-74 bg-white shadow-lg rounded-lg p-8 accesibility-helper min-w-[312px]">
           <div  className="grid gap-4">
             {[
               { icon: KeyOutlined, text: text?.tabs, option: 'helper-core-tabHighlight' },
